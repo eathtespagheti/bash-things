40b548d4ce67db229f18cddebc27979180ad3362
Fabio <sussarellu.fabio@gmail.com>
2018-08-21 12:35:18 +0200

    updated macos scripts

---
+++ firstrun/case-insensitive-autocomplete.sh
@@ -0,0 +1,4 @@
+# Case insensitive auto-complete
+bgecho 'Enabling case insensitive auto-complete...'
+echo "set completion-ignore-case On" >> ~/.inputrc
+echo 'OK'
--- firstrun/firstrun-macos.sh
+++ firstrun/firstrun-macos.sh
@@ -1,17 +1,69 @@
 #!/usr/bin/env bash
+export HOME_PATH='/User'
+ALLTRUE=false
 
-# Enable bash_things
-echo 'Enable bash_things in .bashrc...'
-HOME_PATH='/User'
-echo >> $HOME_PATH/$USER/.bashrc
-echo "# Customization" >> $HOME_PATH/$USER/.bashrc
-echo "HOME_PATH='${HOME_PATH}'" >> $HOME_PATH/$USER/.bashrc
-echo "source ${HOME_PATH}/$USER/.bash_things/loader.sh" >> $HOME_PATH/$USER/.bashrc
+if [[ $1 = '--alltrue' ]]; then
+  ALLTRUE=true
+fi
 
+# Loading echo functions
+source $HOME_PATH/$USER/.bash_things/firstrun/echo.sh
 
-# Case insensitive auto-complete
-echo 'Enabling case insensitive auto-complete...'
-echo "set completion-ignore-case On" >> ~/.inputrc
+if [[ $ALLTRUE = true ]]; then
+  # Enable bash_things
+  bgecho 'Enable bash_things in .bash_profile'
+  echo >> $HOME_PATH/$USER/.bash_profile
+  echo "# Customization" >> $HOME_PATH/$USER/.bash_profile
+  echo "HOME_PATH='${HOME_PATH}'" >> $HOME_PATH/$USER/.bash_profile
+  echo "source ${HOME_PATH}/$USER/.bash_things/loader.sh" >> $HOME_PATH/$USER/.bash_profile
+
+  # Case insensitive autocomplete
+  $HOME_PATH/$USER/.bash_things/firstrun/case-insensitive-autocomplete.sh
+  # NVM
+  $HOME_PATH/$USER/.bash_things/firstrun/nvm.sh
+  # GIT superpush
+  $HOME_PATH/$USER/.bash_things/firstrun/git.sh
+else
+  # Enable bash_things
+  read -p "Enable bash_things? [Y/n] " -n 1 -r
+  if [[ $REPLY =~ ^[Nn]$ ]]
+  then
+    echo
+  else
+    bgecho 'Enable bash_things in .bashrc'
+    echo >> $HOME_PATH/$USER/.bashrc
+    echo "# Customization" >> $HOME_PATH/$USER/.bashrc
+    echo "HOME_PATH='${HOME_PATH}'" >> $HOME_PATH/$USER/.bashrc
+    echo "source ${HOME_PATH}/$USER/.bash_things/loader.sh" >> $HOME_PATH/$USER/.bashrc
+  fi
+
+  # Case insensitive autocomplete
+  read -p "Enable case insensitive autocomplete? [Y/n] " -n 1 -r
+  if [[ $REPLY =~ ^[Nn]$ ]]
+  then
+    echo
+  else
+    $HOME_PATH/$USER/.bash_things/firstrun/case-insensitive-autocomplete.sh
+  fi
+
+  # NVM
+  read -p "Install NVM? [Y/n] " -n 1 -r
+  if [[ $REPLY =~ ^[Nn]$ ]]
+  then
+    echo
+  else
+    $HOME_PATH/$USER/.bash_things/firstrun/nvm.sh
+  fi
+
+  # GIT superpush
+  read -p "Enable GIT superpush? [Y/n] " -n 1 -r
+  if [[ $REPLY =~ ^[Nn]$ ]]
+  then
+    echo
+  else
+    $HOME_PATH/$USER/.bash_things/firstrun/git.sh
+  fi
+fi
 
 # echo Done
-echo 'Done!'
+bgecho 'Done!'

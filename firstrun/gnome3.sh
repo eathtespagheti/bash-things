98bcd3aa856a01c831e988423ed63234ac3de0a9
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2018-08-21 10:24:12 +0200

    Added gnome tweaks

--- firstrun/gnome3.sh
+++ firstrun/gnome3.sh
@@ -1,3 +1,9 @@
+echo
+bgecho '--- GNOME 3 Tweaks ---'
+
 # Fix GNOME 3 workspaces
-echo 'Fixing GNOME 3 workspaces...'
+gecho 'Fixing GNOME 3 workspaces'
 gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
+
+gecho 'Installing Gnome Tweaks'
+sudo apt install gnome-tweak-tool -y

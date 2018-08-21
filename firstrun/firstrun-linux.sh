1ff2b42cc47cbafda484f7e07e0f968b08de6bc9
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2018-08-21 10:24:53 +0200

    added docker to firstrun

--- firstrun/firstrun-linux.sh
+++ firstrun/firstrun-linux.sh
@@ -1,18 +1,34 @@
+# Colors
+Green='\033[0;32m'
+BGreen='\033[1;32m'
+Color_Off='\033[0m' # No Color
+
+# Green echo
+function gecho {
+  echo -e "${Green}$1${Color_Off}"
+}
+function bgecho {
+  echo -e "${BGreen}$1${Color_Off}"
+}
+
 # Enable bash_things
-echo 'Enable bash_things in .bashrc...'
 HOME_PATH='/home'
+bgecho 'Enable bash_things in .bashrc'
 echo >> $HOME_PATH/$USER/.bashrc
 echo "# Customization" >> $HOME_PATH/$USER/.bashrc
 echo "HOME_PATH='${HOME_PATH}'" >> $HOME_PATH/$USER/.bashrc
 echo "source ${HOME_PATH}/$USER/.bash_things/loader.sh" >> $HOME_PATH/$USER/.bashrc
 
 # Package update and upgrade
-echo 'Updating and upgrading packages...'
+gecho 'Updating and upgrading packages'
 sudo apt update
 sudo apt upgrade -y
 
 # Gnome 3 hacks
-.${HOME_PATH}/$USER/.bash_things/firstrun/gnome3.sh
+sh $HOME_PATH/$USER/.bash_things/firstrun/gnome3.sh
+
+# Docker
+sh $HOME_PATH/$USER/.bash_things/firstrun/docker.sh
 
 # echo Done
-echo 'Done!'
+gecho 'Done!'

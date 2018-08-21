a97b8f409bda14cd7849b89b99453714700f3467
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2018-08-21 11:20:51 +0200

    added alltrue parameter

--- firstrun/firstrun-linux.sh
+++ firstrun/firstrun-linux.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 export HOME_PATH='/home'
+ALLTRUE=false
+
+if [[ $1 = '--alltrue' ]]; then
+  ALLTRUE=true
+fi
 
 # Loading echo functions
 source $HOME_PATH/$USER/.bash_things/firstrun/echo.sh
@@ -9,35 +14,49 @@ gecho 'Updating and upgrading packages'
 sudo apt update
 sudo apt upgrade -y
 
-# Enable bash_things
-read -p "Enable bash_things? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]
-then
-  echo
-else
+if [[ $ALLTRUE = true ]]; then
+  # Enable bash_things
   bgecho 'Enable bash_things in .bashrc'
   echo >> $HOME_PATH/$USER/.bashrc
   echo "# Customization" >> $HOME_PATH/$USER/.bashrc
   echo "HOME_PATH='${HOME_PATH}'" >> $HOME_PATH/$USER/.bashrc
   echo "source ${HOME_PATH}/$USER/.bash_things/loader.sh" >> $HOME_PATH/$USER/.bashrc
-fi
 
-# Gnome 3 hacks
-read -p "Enable Gnome 3 Hacks? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]
-then
-  echo
-else
+  # Gnome 3 hacks
   $HOME_PATH/$USER/.bash_things/firstrun/gnome3.sh
-fi
-
-# Docker
-read -p "Install Docker? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]
-then
-  echo
-else
+  # Docker
   $HOME_PATH/$USER/.bash_things/firstrun/docker.sh
+else
+  # Enable bash_things
+  read -p "Enable bash_things? [Y/n] " -n 1 -r
+  if [[ $REPLY =~ ^[Nn]$ ]]
+  then
+    echo
+  else
+    bgecho 'Enable bash_things in .bashrc'
+    echo >> $HOME_PATH/$USER/.bashrc
+    echo "# Customization" >> $HOME_PATH/$USER/.bashrc
+    echo "HOME_PATH='${HOME_PATH}'" >> $HOME_PATH/$USER/.bashrc
+    echo "source ${HOME_PATH}/$USER/.bash_things/loader.sh" >> $HOME_PATH/$USER/.bashrc
+  fi
+
+  # Gnome 3 hacks
+  read -p "Enable Gnome 3 Hacks? [Y/n] " -n 1 -r
+  if [[ $REPLY =~ ^[Nn]$ ]]
+  then
+    echo
+  else
+    $HOME_PATH/$USER/.bash_things/firstrun/gnome3.sh
+  fi
+
+  # Docker
+  read -p "Install Docker? [Y/n] " -n 1 -r
+  if [[ $REPLY =~ ^[Nn]$ ]]
+  then
+    echo
+  else
+    $HOME_PATH/$USER/.bash_things/firstrun/docker.sh
+  fi
 fi
 
 # echo Done

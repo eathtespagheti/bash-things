27fc4249142b6e913a7526b09d6e3b488be1dec3
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2018-08-21 17:19:40 +0200

    added ultrapush

--- firstrun/git.sh
+++ firstrun/git.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 bgecho '--- Enabling GIT superpush ---'
-git config --global alias.superpush '!git remote | xargs -L1 git push --all'
+git config --global alias.ultrapush '!git remote | xargs -L1 git push --all'
+git config --global alias.superpush '!git remote | xargs -L1 git push'

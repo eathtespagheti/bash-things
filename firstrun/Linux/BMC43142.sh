e4170407b106f32d97ea8e4cc076aa02f73020ac
Fabio Sussarellu <fabio@amd-stark-portable.lan>
2018-10-15 16:14:08 +0200

    FIX bluetooth driver script

--- firstrun/Linux/BMC43142.sh
+++ firstrun/Linux/BMC43142.sh
@@ -4,4 +4,5 @@ gecho 'Installing WiFi Drivers'
 sudo apt --reinstall install bcmwl-kernel-source -y
 
 gecho 'Downloading bluetooth firmware for BCM43142A0'
-sudo wget https://github.com/winterheart/broadcom-bt-firmware/raw/master/brcm/BCM43142A0-0a5c-216c.hcd /lib/firmware/brcm
+wget https://github.com/winterheart/broadcom-bt-firmware/raw/master/brcm/BCM43142A0-0a5c-216c.hcd
+sudo mv BCM43142A0-0a5c-216c.hcd /lib/firmware/brcm/BCM43142A0-0a5c-216c.hcd

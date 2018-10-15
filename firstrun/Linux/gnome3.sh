e40753e5d8523fd2a287be6f6c5c4a3298da1690
Fabio Sussarellu <fabio@amd-stark-portable.lan>
2018-10-15 16:46:12 +0200

    ADD workspace on multimonitor fix

--- firstrun/Linux/gnome3.sh
+++ firstrun/Linux/gnome3.sh
@@ -5,3 +5,6 @@ bgecho '--- GNOME 3 Tweaks ---'
 
 gecho 'Installing Gnome Tweaks'
 sudo apt install gnome-tweak-tool -y
+
+gecho 'Fixing Multimonitor Workspaces'
+gsettings set org.gnome.shell.overrides workspaces-only-on-primary false

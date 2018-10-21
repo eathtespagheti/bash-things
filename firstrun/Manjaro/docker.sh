6a06943669eae4f5dceae49e999dca8a91168c30
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2018-10-21 13:20:54 +0200

    ADD docker script

---
+++ firstrun/Manjaro/docker.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo
+bgecho '--- DOCKER INSTALL ---'
+
+gecho 'Installing Docker CE'
+$PACKAGE_INSTALL docker $AUTOYES
+
+echo
+bgecho 'Enabling Docker as a non root user'
+gecho 'Adding docker group'
+sudo groupadd docker
+echo 'OK'
+gecho "Adding ${USER} to the docker group"
+sudo usermod -aG docker $USER
+echo 'OK'
+
+echo
+gecho 'Enable Docker at startup'
+sudo systemctl enable docker
+echo 'OK'
+
+echo
+gecho 'Installing docker-compose'
+pip install docker-compose

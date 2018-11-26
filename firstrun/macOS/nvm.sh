511857b2d4e8933548370b3d0c881efd81733d93
Fabio Sussarellu <fabio@LittleChameleon.lan>
2018-11-26 15:31:50 +0100

    update nvm script

---
+++ firstrun/macOS/nvm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+bgecho '--- Node Version Manager ---'
+gecho 'Sourcing .bashrc in .bash_profile'
+echo "source ~/.bashrc" >> $HOME_PATH/$USER/.bash_profile
+gecho 'Executing official install script'
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+gecho 'Reloading .bashrc'
+source $HOME_PATH/$USER/.bashrc
+echo 'OK'
+gecho 'Installing latest node'
+nvm install node
+gecho 'Enabling latest node'
+nvm use node

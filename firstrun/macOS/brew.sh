359a7e286c95bfa351575193b7584aab263e5c38
Fabio Sussarellu <fabio@LittleChameleon.lan>
2018-11-26 15:36:30 +0100

    ADD homebrew install script

---
+++ firstrun/macOS/brew.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+bgecho '--- Homebrew Package Manager ---'
+gecho 'Executing official install script'
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+gecho 'Reloading .bash_profile'
+source $HOME_PATH/$USER/.bash_profile
+echo 'OK'

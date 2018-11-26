f810c6968ab4a9357387d87074f2f5f11e6f7712
Fabio Sussarellu <fabio@LittleChameleon.lan>
2018-11-26 15:36:18 +0100

    FIX bash profile load

--- firstrun/macOS/nvm.sh
+++ firstrun/macOS/nvm.sh
@@ -5,8 +5,8 @@ gecho 'Sourcing .bashrc in .bash_profile'
 echo "source ~/.bashrc" >> $HOME_PATH/$USER/.bash_profile
 gecho 'Executing official install script'
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-gecho 'Reloading .bashrc'
-source $HOME_PATH/$USER/.bashrc
+gecho 'Reloading .bash_profile'
+source $HOME_PATH/$USER/.bash_profile
 echo 'OK'
 gecho 'Installing latest node'
 nvm install node

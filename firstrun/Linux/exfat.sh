866488d34def94bcbea125254d52b9ca94b8aabd
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-03-22 16:04:41 +0000

    REMOVE ubuntu and manjaro scripts

--- firstrun/Linux/exfat.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-gecho 'Installing exfat drivers'
-sudo apt install exfat-fuse exfat-utils -y
-echo 'OK'
--- firstrun/Linux/update-manager.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo
-bgecho '--- Ubuntu Update Manager ---'
-
-gecho 'Installing Ubuntu Update Manager'
-sudo apt install update-manager -y

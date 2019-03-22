ddf293fd05a0f2cee655d740e53513f3d4dec904
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-03-22 15:50:19 +0000

    ADD dkms

--- firstrun/Arch/broadcom.sh
+++ firstrun/Arch/broadcom.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 gecho 'Installing linux-headers'
-$PACKAGE_INSTALL linux-headers $AUTOYES
+$PACKAGE_INSTALL linux-headers dkms $AUTOYES
 
 gecho 'Installing WiFi Drivers'
 $PACKAGE_INSTALL broadcom-wl-dkms $AUTOYES

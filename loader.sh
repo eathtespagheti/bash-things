1f0afb1b7e631901d6a472b332afaffeb019a544
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-03-22 16:05:55 +0000

    CHANGE home path

--- loader.sh
+++ loader.sh
@@ -2,11 +2,11 @@
 
 MACHINE=''
 # get colors
-source $HOME_PATH/$USER/.bash_things/text/colors.sh
+source $HOME_PATH/.bash_things/text/colors.sh
 # get fancy
-source $HOME_PATH/$USER/.bash_things/text/bash_string.sh
+source $HOME_PATH/.bash_things/text/bash_string.sh
 # get smart
-source $HOME_PATH/$USER/.bash_things/functions/docker.sh
+source $HOME_PATH/.bash_things/functions/docker.sh
 
 # use nano
 export EDITOR=nano

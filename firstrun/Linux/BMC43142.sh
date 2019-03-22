3b169ea164fee6308b7bc8d9ce15339be05f49cb
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-03-22 15:55:15 +0000

    UPDATE broadcom bluetooth fix

--- firstrun/Linux/BMC43142.sh
+++ firstrun/Linux/BMC43142.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-gecho 'Installing WiFi Drivers'
-sudo apt --reinstall install bcmwl-kernel-source -y
-
 gecho 'Downloading bluetooth firmware for BCM43142A0'
-wget https://github.com/winterheart/broadcom-bt-firmware/raw/master/brcm/BCM43142A0-0a5c-216c.hcd
-sudo mv BCM43142A0-0a5c-216c.hcd /lib/firmware/brcm/BCM43142A0-0a5c-216c.hcd
+sudo wget https://github.com/winterheart/broadcom-bt-firmware/raw/master/brcm/BCM43142A0-0a5c-216c.hcd -P /lib/firmware/brcm/

01ce386d1dbd9706b1a8d0d1a817a86e8c4e7a57
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-09-05 17:40:50 +0200

    Add trueline

--- loader.sh
+++ loader.sh
@@ -2,11 +2,11 @@
 
 MACHINE=''
 # get colors
-source $HOME_PATH/.bash_things/text/colors.sh
+# source $HOME_PATH/.bash_things/text/colors.sh
 # get fancy
-source $HOME_PATH/.bash_things/text/bash_string.sh
+# source $HOME_PATH/.bash_things/text/bash_string.sh
 # get smart
-source $HOME_PATH/.bash_things/functions/docker.sh
+# source $HOME_PATH/.bash_things/functions/docker.sh
 
 # use nano
 export EDITOR=nano
@@ -16,3 +16,10 @@ export MAKEFLAGS='-j8'
 
 # add python user bin to path
 export PATH=$PATH:~/.local/bin
+
+# custom yay alias
+alias yy="yay -Syyu --noconfirm"
+
+# trueline settings
+source $HOME_PATH/.bash_things/trueline-settings.sh
+source $HOME_PATH/.bash_things/trueline.sh
---
+++ trueline-settings.sh
@@ -0,0 +1,27 @@
+declare -a TRUELINE_SEGMENTS=(
+    'working_dir,special_grey,orange'
+    'git,special_grey,green'
+    'exit_status,white,special_grey'
+    'time,white,black'
+    #'newline,white,special_grey'
+)
+
+declare -A TRUELINE_SYMBOLS=(
+    [working_dir_home]='~'
+    [clock]='🕒'
+)
+
+TRUELINE_GIT_MODIFIED_COLOR='black'
+TRUELINE_USER_SHOW_IP_SSH=true
+
+_trueline_time_segment() {
+    local prompt_time="${TRUELINE_SYMBOLS[clock]} \t"
+    if [[ -n "$prompt_time" ]]; then
+        local fg_color="$1"
+        local bg_color="$2"
+        local segment="$(_trueline_separator)"
+        segment+="$(_trueline_content "$fg_color" "$bg_color" normal " $prompt_time ")"
+        PS1+="$segment"
+        _last_color=$bg_color
+    fi
+}

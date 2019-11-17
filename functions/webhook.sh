4d820eaac40ae21577b6d065af4c230c42292d91
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-11-17 21:59:30 +0100

    configured webhook with config file

--- functions/webhook.sh
+++ functions/webhook.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+function checkKey() {
+    if [ -z "$1" ]; then # Check if the KEY parameter exist
+        echo "No KEY provided, using the default one..."
+        source $HOME_PATH/.webhook/config
+    fi
+    if [ -z "$KEY" ]; then # Check if the KEY parameter exist
+        echo "No KEY configured in the configuration file"
+        echo "Write your KEY as a bash variable inside" $HOME_PATH"/.webhook/config"
+        exit 3
+    fi
+}
+
 if [ -z "$1" ]; then # Check turn status
     echo "No action provided"
     echo "Usage: webhook.sh [0/1] [DEVICE] [KEY]"
@@ -10,10 +22,10 @@ elif [ -z "$2" ]; then # Check if the DEVICE parameter exist
     echo "No DEVICE provided"
     echo "Usage: webhook.sh [0/1] [DEVICE] [KEY]"
     echo "If no key it's provided the default will be used"
-    exit 1
+    exit 2
 elif [ -z "$3" ]; then # Check if the KEY parameter exist
     SUBMISSION_NUMBER=$1
-    echo "No KEY provided, using the default one"
+    checkKey $3
     COMMAND=$1
     DEVICE=$2
 else
@@ -23,22 +35,21 @@ else
 fi
 
 if [ "$COMMAND" = "1" ]; then
-     COMMAND="on"
+    COMMAND="on"
 elif [ "$COMMAND" = "0" ]; then
-     COMMAND="off"
+    COMMAND="off"
 else
     echo "Wrong command provided"
     echo "Valid values are: 0 ; 1"
     exit 1
 fi
 
-
 MESSAGE="turn_${COMMAND}_${DEVICE}"
 REQUEST="https://maker.ifttt.com/trigger/${MESSAGE}/with/key/${KEY}"
 
 echo "Turning $COMMAND $DEVICE with key $KEY"
 
-wget -O result $REQUEST > /dev/null 2>&1 # Make web request and suppress output
-cat result # Print request result
+wget -O result $REQUEST >/dev/null 2>&1 # Make web request and suppress output
+cat result                              # Print request result
 echo
 rm result # Remove request result

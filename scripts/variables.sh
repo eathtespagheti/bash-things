eb3a4197a35363fadf87e8e2fdaca00e81e22fa0
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-11-27 13:34:07 +0100

    remove macOS check from variables

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# macOS check
-source $bashthingsFunctions/checkOS.sh
-if [ "$machine" = "Mac" ]; then
-    export HOME="/Users/${USER}"
-fi
-
 export bashthingsFolder=$HOME/.bash_things
 export bashthingsFunctions=$bashthingsFolder/functions
 export bashthingsScripts=$bashthingsFolder/scripts

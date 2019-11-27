84a135fbf5d257f1619aed7ea81dafc63ec987eb
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-11-27 18:20:44 +0100

    ADD diffrent read

--- firstrun/firstrun-arch.sh
+++ firstrun/firstrun-arch.sh
@@ -27,137 +27,137 @@ makepkg -si $AUTOYES
 cd ..
 rm -rf yay
 
-# if [[ $ALLTRUE == true ]]; then
-#   # Enable bash_things
-#   bgecho 'Enable bash_things in .bashrc'
-#   echo >>$HOME/.bashrc
-#   echo "# Customization" >>$HOME/.bashrc
-#   echo "source ${bashthingsFolder}/loader.sh" >>$HOME/.bashrc
-
-#   # Case insensitive autocomplete
-#   $bashthingsFirstrun/case-insensitive-autocomplete.sh
-#   # Improved Graphic Drivers
-#   $bashthingsFirstrunSpecific/mesa.sh
-#   # Dev-Tools
-#   $bashthingsFirstrunSpecific/dev-tools.sh
-#   # Broadcom WiFi
-#   $bashthingsFirstrunSpecific/broadcom.sh
-#   # BMC43142 Bluetooth fix
-#   $bashthingsFirstrun/BMC43142.sh
-#   # Docker
-#   $bashthingsFirstrunSpecific/docker.sh
-#   # docker-machine
-#   $bashthingsFirstrunSpecific/docker-machine.sh
-#   # NVM
-#   $bashthingsFirstrunSpecific/nvm.sh
-#   # GIT superpush
-#   $bashthingsFirstrun/git.sh
-#   # screen-sleep
-#   $bashthingsFirstrunSpecific/screen-sleep.sh
-#   # lolcat
-#   $bashthingsFirstrunSpecific/lolcat.sh
-# else
-# Enable bash_things
-read -p "Enable bash_things? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-  echo
-else
+if [[ $ALLTRUE == true ]]; then
   # Enable bash_things
   bgecho 'Enable bash_things in .bashrc'
   echo >>$HOME/.bashrc
   echo "# Customization" >>$HOME/.bashrc
   echo "source ${bashthingsFolder}/loader.sh" >>$HOME/.bashrc
-fi
 
-# Case insensitive autocomplete
-read -p "Enable case insensitive autocomplete? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-  echo
-else
+  # Case insensitive autocomplete
   $bashthingsFirstrun/case-insensitive-autocomplete.sh
-fi
-
-# Improved Graphic Drivers
-read -p "Enable Improved Graphic Drivers? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-  echo
-else
+  # Improved Graphic Drivers
   $bashthingsFirstrunSpecific/mesa.sh
-fi
-
-# Dev-Tools
-read -p "Install Dev-Tools? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-  echo
-else
+  # Dev-Tools
   $bashthingsFirstrunSpecific/dev-tools.sh
-fi
-
-# Broadcom WiFi
-read -p "Install Broadcom WiFi Drivers? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-  echo
-else
+  # Broadcom WiFi
   $bashthingsFirstrunSpecific/broadcom.sh
-fi
-
-# BMC43142 Bluetooth fix
-read -p "Fix Broadcom BMC43142 Bluetooth? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-  echo
-else
+  # BMC43142 Bluetooth fix
   $bashthingsFirstrun/BMC43142.sh
-fi
-
-# Docker
-read -p "Install Docker? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-  echo
-else
+  # Docker
   $bashthingsFirstrunSpecific/docker.sh
-fi
-
-# docker-machine
-read -p "Install docker-machine? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-  echo
-else
+  # docker-machine
   $bashthingsFirstrunSpecific/docker-machine.sh
-fi
-
-# NVM
-read -p "Install NVM? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-  echo
-else
+  # NVM
   $bashthingsFirstrunSpecific/nvm.sh
-fi
-
-# GIT superpush
-read -p "Enable GIT superpush? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-  echo
-else
+  # GIT superpush
   $bashthingsFirstrun/git.sh
-fi
-
-# screen-sleep
-read -p "Install screen-sleep? [Y/n] " -n 1 -r
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-  echo
-else
+  # screen-sleep
   $bashthingsFirstrunSpecific/screen-sleep.sh
-fi
+  # lolcat
+  $bashthingsFirstrunSpecific/lolcat.sh
+else
+  # Enable bash_things
+  read secondyn -p "Enable bash_things? [Y/n] " -n 1 -r </dev/tty
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo
+  else
+    # Enable bash_things
+    bgecho 'Enable bash_things in .bashrc'
+    echo >>$HOME/.bashrc
+    echo "# Customization" >>$HOME/.bashrc
+    echo "source ${bashthingsFolder}/loader.sh" >>$HOME/.bashrc
+  fi
 
-# lolcat
-$bashthingsFirstrunSpecific/lolcat.sh
-#fi
+  # Case insensitive autocomplete
+  read secondyn -p "Enable case insensitive autocomplete? [Y/n] " -n 1 -r </dev/tty
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo
+  else
+    $bashthingsFirstrun/case-insensitive-autocomplete.sh
+  fi
+
+  # Improved Graphic Drivers
+  read secondyn -p "Enable Improved Graphic Drivers? [Y/n] " -n 1 -r </dev/tty
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo
+  else
+    $bashthingsFirstrunSpecific/mesa.sh
+  fi
+
+  # Dev-Tools
+  read secondyn -p "Install Dev-Tools? [Y/n] " -n 1 -r </dev/tty
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo
+  else
+    $bashthingsFirstrunSpecific/dev-tools.sh
+  fi
+
+  # Broadcom WiFi
+  read secondyn -p "Install Broadcom WiFi Drivers? [Y/n] " -n 1 -r </dev/tty
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo
+  else
+    $bashthingsFirstrunSpecific/broadcom.sh
+  fi
+
+  # BMC43142 Bluetooth fix
+  read secondyn -p "Fix Broadcom BMC43142 Bluetooth? [Y/n] " -n 1 -r </dev/tty
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo
+  else
+    $bashthingsFirstrun/BMC43142.sh
+  fi
+
+  # Docker
+  read secondyn -p "Install Docker? [Y/n] " -n 1 -r </dev/tty
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo
+  else
+    $bashthingsFirstrunSpecific/docker.sh
+  fi
+
+  # docker-machine
+  read secondyn -p "Install docker-machine? [Y/n] " -n 1 -r </dev/tty
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo
+  else
+    $bashthingsFirstrunSpecific/docker-machine.sh
+  fi
+
+  # NVM
+  read secondyn -p "Install NVM? [Y/n] " -n 1 -r </dev/tty
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo
+  else
+    $bashthingsFirstrunSpecific/nvm.sh
+  fi
+
+  # GIT superpush
+  read secondyn -p "Enable GIT superpush? [Y/n] " -n 1 -r </dev/tty
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo
+  else
+    $bashthingsFirstrun/git.sh
+  fi
+
+  # screen-sleep
+  read secondyn -p "Install screen-sleep? [Y/n] " -n 1 -r </dev/tty
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo
+  else
+    $bashthingsFirstrunSpecific/screen-sleep.sh
+  fi
+
+  # lolcat
+  $bashthingsFirstrunSpecific/lolcat.sh
+fi
 
 # echo Done
 bgecho 'Done!'
 
 # Final reboot
-# read -p "Reboot system now? [Y/n] " -n 1 -r
+# read secondyn </dev/tty -p "Reboot system now? [Y/n] " -n 1 -r
 # if [[ $REPLY =~ ^[Nn]$ ]]; then
 #   echo
 # else

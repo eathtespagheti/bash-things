01505b1bc3ea72c75ed90c71fca7f98260c17d3b
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-11-27 13:03:52 +0100

    ADD checkOS

---
+++ functions/checkOS.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac

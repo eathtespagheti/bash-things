bdb77f4828584c8fe7a5d9b2d394d8b00eecfe1b
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-11-27 18:06:09 +0100

    disable allyes

--- firstrun/firstrun-arch.sh
+++ firstrun/firstrun-arch.sh
@@ -27,132 +27,131 @@ makepkg -si $AUTOYES
 cd ..
 rm -rf yay
 
-if [[ $ALLTRUE == true ]]; then
+# if [[ $ALLTRUE == true ]]; then
+#   # Enable bash_things
+#   bgecho 'Enable bash_things in .bashrc'
+#   echo >>$HOME/.bashrc
+#   echo "# Customization" >>$HOME/.bashrc
+#   echo "source ${bashthingsFolder}/loader.sh" >>$HOME/.bashrc
+
+#   # Case insensitive autocomplete
+#   $bashthingsFirstrun/case-insensitive-autocomplete.sh
+#   # Improved Graphic Drivers
+#   $bashthingsFirstrunSpecific/mesa.sh
+#   # Dev-Tools
+#   $bashthingsFirstrunSpecific/dev-tools.sh
+#   # Broadcom WiFi
+#   $bashthingsFirstrunSpecific/broadcom.sh
+#   # BMC43142 Bluetooth fix
+#   $bashthingsFirstrun/BMC43142.sh
+#   # Docker
+#   $bashthingsFirstrunSpecific/docker.sh
+#   # docker-machine
+#   $bashthingsFirstrunSpecific/docker-machine.sh
+#   # NVM
+#   $bashthingsFirstrunSpecific/nvm.sh
+#   # GIT superpush
+#   $bashthingsFirstrun/git.sh
+#   # screen-sleep
+#   $bashthingsFirstrunSpecific/screen-sleep.sh
+#   # lolcat
+#   $bashthingsFirstrunSpecific/lolcat.sh
+# else
+# Enable bash_things
+read -p "Enable bash_things? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
   # Enable bash_things
   bgecho 'Enable bash_things in .bashrc'
   echo >>$HOME/.bashrc
   echo "# Customization" >>$HOME/.bashrc
   echo "source ${bashthingsFolder}/loader.sh" >>$HOME/.bashrc
-
-  # Case insensitive autocomplete
-  $bashthingsFirstrun/case-insensitive-autocomplete.sh
-  # Improved Graphic Drivers
-  $bashthingsFirstrunSpecific/mesa.sh
-  # Dev-Tools
-  $bashthingsFirstrunSpecific/dev-tools.sh
-  # Broadcom WiFi
-  $bashthingsFirstrunSpecific/broadcom.sh
-  # BMC43142 Bluetooth fix
-  $bashthingsFirstrun/BMC43142.sh
-  # Docker
-  $bashthingsFirstrunSpecific/docker.sh
-  # docker-machine
-  $bashthingsFirstrunSpecific/docker-machine.sh
-  # NVM
-  $bashthingsFirstrunSpecific/nvm.sh
-  # GIT superpush
-  $bashthingsFirstrun/git.sh
-  # screen-sleep
-  $bashthingsFirstrunSpecific/screen-sleep.sh
-  # lolcat
-  $bashthingsFirstrunSpecific/lolcat.sh
-else
-
-  # Enable bash_things
-  read -p "Enable bash_things? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    # Enable bash_things
-    bgecho 'Enable bash_things in .bashrc'
-    echo >>$HOME/.bashrc
-    echo "# Customization" >>$HOME/.bashrc
-    echo "source ${bashthingsFolder}/loader.sh" >>$HOME/.bashrc
-  fi
-
-  # Case insensitive autocomplete
-  read -p "Enable case insensitive autocomplete? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrun/case-insensitive-autocomplete.sh
-  fi
-
-  # Improved Graphic Drivers
-  read -p "Enable Improved Graphic Drivers? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/mesa.sh
-  fi
-
-  # Dev-Tools
-  read -p "Install Dev-Tools? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/dev-tools.sh
-  fi
-
-  # Broadcom WiFi
-  read -p "Install Broadcom WiFi Drivers? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/broadcom.sh
-  fi
-
-  # BMC43142 Bluetooth fix
-  read -p "Fix Broadcom BMC43142 Bluetooth? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrun/BMC43142.sh
-  fi
-
-  # Docker
-  read -p "Install Docker? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/docker.sh
-  fi
-
-  # docker-machine
-  read -p "Install docker-machine? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/docker-machine.sh
-  fi
-
-  # NVM
-  read -p "Install NVM? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/nvm.sh
-  fi
-
-  # GIT superpush
-  read -p "Enable GIT superpush? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrun/git.sh
-  fi
-
-  # screen-sleep
-  read -p "Install screen-sleep? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/screen-sleep.sh
-  fi
-
-  # lolcat
-  $bashthingsFirstrunSpecific/lolcat.sh
 fi
+
+# Case insensitive autocomplete
+read -p "Enable case insensitive autocomplete? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrun/case-insensitive-autocomplete.sh
+fi
+
+# Improved Graphic Drivers
+read -p "Enable Improved Graphic Drivers? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/mesa.sh
+fi
+
+# Dev-Tools
+read -p "Install Dev-Tools? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/dev-tools.sh
+fi
+
+# Broadcom WiFi
+read -p "Install Broadcom WiFi Drivers? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/broadcom.sh
+fi
+
+# BMC43142 Bluetooth fix
+read -p "Fix Broadcom BMC43142 Bluetooth? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrun/BMC43142.sh
+fi
+
+# Docker
+read -p "Install Docker? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/docker.sh
+fi
+
+# docker-machine
+read -p "Install docker-machine? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/docker-machine.sh
+fi
+
+# NVM
+read -p "Install NVM? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/nvm.sh
+fi
+
+# GIT superpush
+read -p "Enable GIT superpush? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrun/git.sh
+fi
+
+# screen-sleep
+read -p "Install screen-sleep? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/screen-sleep.sh
+fi
+
+# lolcat
+$bashthingsFirstrunSpecific/lolcat.sh
+#fi
 
 # echo Done
 bgecho 'Done!'
--- firstrun/firstrun-macos.sh
+++ firstrun/firstrun-macos.sh
@@ -15,112 +15,112 @@ if [ "$1" = "--alltrue" ]; then
   ALLTRUE=true
 fi
 
-if [[ $ALLTRUE == true ]]; then
-  # Enable bash_things
+# if [[ $ALLTRUE == true ]]; then
+#   # Enable bash_things
+#   bgecho 'Enable bash_things in .bash_profile'
+#   echo >>$HOME/.bash_profile
+#   echo "# Customization" >>$HOME/.bash_profile
+#   echo "HOME='${HOME}'" >>$HOME/.bash_profile
+#   echo "source ${bashthingsFolder}/loader.sh" >>$HOME/.bash_profile
+
+#   # Case insensitive autocomplete
+#   $bashthingsFirstrun/case-insensitive-autocomplete.sh
+#   # Homebrew
+#   $bashthingsFirstrunSpecific/brew.sh
+#   # Bash upgrade
+#   $bashthingsFirstrunSpecific/bash-upgrade.sh
+#   # bash-completion
+#   $bashthingsFirstrunSpecific/bash-completion.sh
+#   # brew-git
+#   $bashthingsFirstrunSpecific/brew-git.sh
+#   # NVM
+#   $bashthingsFirstrunSpecific/nvm.sh
+#   # GIT superpush
+#   $bashthingsFirstrun/git.sh
+#   # docker-machine autocomplete
+#   $bashthingsFirstrunSpecific/docker-machine.sh
+#   # lolcat
+#   $bashthingsFirstrunSpecific/lolcat.sh
+# else
+# Enable bash_things
+read -p "Enable bash_things? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
   bgecho 'Enable bash_things in .bash_profile'
-  echo >>$HOME/.bash_profile
+  echo >>$HOME/.bashrc
   echo "# Customization" >>$HOME/.bash_profile
   echo "HOME='${HOME}'" >>$HOME/.bash_profile
-  echo "source ${bashthingsFolder}/loader.sh" >>$HOME/.bash_profile
-
-  # Case insensitive autocomplete
-  $bashthingsFirstrun/case-insensitive-autocomplete.sh
-  # Homebrew
-  $bashthingsFirstrunSpecific/brew.sh
-  # Bash upgrade
-  $bashthingsFirstrunSpecific/bash-upgrade.sh
-  # bash-completion
-  $bashthingsFirstrunSpecific/bash-completion.sh
-  # brew-git
-  $bashthingsFirstrunSpecific/brew-git.sh
-  # NVM
-  $bashthingsFirstrunSpecific/nvm.sh
-  # GIT superpush
-  $bashthingsFirstrun/git.sh
-  # docker-machine autocomplete
-  $bashthingsFirstrunSpecific/docker-machine.sh
-  # lolcat
-  $bashthingsFirstrunSpecific/lolcat.sh
-else
-  # Enable bash_things
-  read -p "Enable bash_things? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    bgecho 'Enable bash_things in .bash_profile'
-    echo >>$HOME/.bashrc
-    echo "# Customization" >>$HOME/.bash_profile
-    echo "HOME='${HOME}'" >>$HOME/.bash_profile
-    echo "source ${bashThingsFolder}/loader.sh" >>$HOME/.bash_profile
-  fi
-
-  # Case insensitive autocomplete
-  read -p "Enable case insensitive autocomplete? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrun/case-insensitive-autocomplete.sh
-  fi
-
-  # Homebrew
-  read -p "Install Homebrew? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/brew.sh
-  fi
-
-  # Bash upgrade
-  read -p "Upgrade bash? (requires homebrew installed) [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/bash-upgrade.sh
-  fi
-
-  # bash-completion
-  read -p "Install bash-completion? (requires homebrew installed) [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/bash-completion.sh
-  fi
-
-  # brew-git
-  read -p "Reinstall git via brew? (requires homebrew installed) [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/brew-git.sh
-  fi
-
-  # NVM
-  read -p "Install NVM? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/nvm.sh
-  fi
-
-  # GIT superpush
-  read -p "Enable GIT superpush? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrun/git.sh
-  fi
-
-  # docker-machine autocomplete
-  read -p "Enable docker-machine autocomplete? [Y/n] " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo
-  else
-    $bashthingsFirstrunSpecific/docker-machine.sh
-  fi
-
-  # lolcat
-  $bashthingsFirstrunSpecific/lolcat.sh
+  echo "source ${bashThingsFolder}/loader.sh" >>$HOME/.bash_profile
 fi
+
+# Case insensitive autocomplete
+read -p "Enable case insensitive autocomplete? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrun/case-insensitive-autocomplete.sh
+fi
+
+# Homebrew
+read -p "Install Homebrew? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/brew.sh
+fi
+
+# Bash upgrade
+read -p "Upgrade bash? (requires homebrew installed) [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/bash-upgrade.sh
+fi
+
+# bash-completion
+read -p "Install bash-completion? (requires homebrew installed) [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/bash-completion.sh
+fi
+
+# brew-git
+read -p "Reinstall git via brew? (requires homebrew installed) [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/brew-git.sh
+fi
+
+# NVM
+read -p "Install NVM? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/nvm.sh
+fi
+
+# GIT superpush
+read -p "Enable GIT superpush? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrun/git.sh
+fi
+
+# docker-machine autocomplete
+read -p "Enable docker-machine autocomplete? [Y/n] " -n 1 -r
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo
+else
+  $bashthingsFirstrunSpecific/docker-machine.sh
+fi
+
+# lolcat
+$bashthingsFirstrunSpecific/lolcat.sh
+# fi
 
 # echo Done
 bgecho 'Done!'

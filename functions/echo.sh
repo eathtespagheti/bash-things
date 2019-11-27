c73449e4eb8995f81c5a7ecd295daf95df954608
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-11-27 12:52:25 +0100

    ADD better colors configuration

--- functions/echo.sh
+++ functions/echo.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 
 # Colors
-export Green='\033[0;32m'
-export BGreen='\033[1;32m'
-export BRed='\033[1;32m'
-export Color_Off='\033[0m' # No Color
+export C_GREEN='\033[38;5;40m'
+export C_RED="\033[38;5;9m"
+export F_BOLD="\033[1m"
+export C_NO_COLOR='\033[0m' # No Color
 
 function gecho { # Green echo
-  echo -e "${Green}$1${Color_Off}"
+  echo -e "${C_GREEN}$1${C_NO_COLOR}"
+}
+
+function recho { # Red echo
+  echo -e "${C_RED}$1${C_NO_COLOR}"
 }
 
 function bgecho { # Bold Green echo
-  echo -e "${BGreen}$1${Color_Off}"
+  echo -e "${F_BOLD}${C_GREEN}$1${C_NO_COLOR}"
 }
-function brecho { # Bold Red echo
-  echo -e "${BRed}$1${Color_Off}"
+
+function bgecho { # Bold Red echo
+  echo -e "${F_BOLD}${C_RED}$1${C_NO_COLOR}"
 }
 
 bgecho '--- EXPORTING ECHO FUNCTIONS ---'
 export -f gecho
+export -f recho
 export -f bgecho
 export -f brecho

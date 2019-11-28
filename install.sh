ab0303cd405f65550d126ce140c46833474ef61c
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-11-28 16:28:34 +0100

    UPDATE trueline location for scriptsfor

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Clone bash things"
 git clone https://gitlab.com/Fabi0Z/bash-things.git .bash_things
 echo
 echo "Clone trueline"
-git clone https://github.com/petobens/trueline.git .bash_things/trueline
+git clone https://github.com/petobens/trueline.git .bash_things/modules/trueline
 echo
 echo "Clone webhook"
 git clone https://gitlab.com/Fabi0Z/webhook.git .bash_things/scripts/webhook

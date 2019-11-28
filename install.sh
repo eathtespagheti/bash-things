2f6ceae8e2a56597532af7146b22f05a81ce67c5
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2019-11-28 16:22:21 +0100

    UPDATE webhook install location

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "Clone trueline"
 git clone https://github.com/petobens/trueline.git .bash_things/trueline
 echo
 echo "Clone webhook"
-git clone https://gitlab.com/Fabi0Z/webhook.git .bash_things/script/webhook
+git clone https://gitlab.com/Fabi0Z/webhook.git .bash_things/scripts/webhook
 echo
 echo "Execute firstrun script"
 ./.bash_things/firstrun.sh

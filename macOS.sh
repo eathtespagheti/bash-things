157d65835eb218ec95f127bc4970c4ead36bec27
Fabio Sussarellu <fabio@Big-Chameleon.local>
2019-12-03 17:37:38 +0100

    ADD compinit

--- macOS.sh
+++ macOS.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-# zsh-completions
+# zsh-completion
 fpath=(/usr/local/share/zsh-completions $fpath)
 
 # syntax highlighting
@@ -9,3 +9,7 @@ source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 # pure loading
 source $bashthingsModules/pure.sh
 prompt pure
+
+# initialize completions
+autoload -Uz compinit
+compinit

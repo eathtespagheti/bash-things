bfb0bfcce1598998c5b5509c4cc80859d5e38fe1
Fabio Sussarellu <fabio@Big-Chameleon.local>
2019-12-03 11:49:58 +0100

    UPDATE echo for zsh

--- functions/echo.sh
+++ functions/echo.sh
@@ -6,23 +6,23 @@ export C_RED="\033[38;5;9m"
 export F_BOLD="\033[1m"
 export C_NO_COLOR='\033[0m' # No Color
 
-function gecho { # Green echo
+function gecho() { # Green echo
   echo -e "${C_GREEN}$1${C_NO_COLOR}"
 }
 
-function recho { # Red echo
+function recho() { # Red echo
   echo -e "${C_RED}$1${C_NO_COLOR}"
 }
 
-function bgecho { # Bold Green echo
+function bgecho() { # Bold Green echo
   echo -e "${F_BOLD}${C_GREEN}$1${C_NO_COLOR}"
 }
 
-function brecho { # Bold Red echo
+function brecho() { # Bold Red echo
   echo -e "${F_BOLD}${C_RED}$1${C_NO_COLOR}"
 }
 
-export -f gecho
-export -f recho
-export -f bgecho
-export -f brecho
+export -f gecho >/dev/null
+export -f recho >/dev/null
+export -f bgecho >/dev/null
+export -f brecho >/dev/null

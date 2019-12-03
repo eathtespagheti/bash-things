d7379419751fa7b0f4aa018501cc09057291e047
Fabio Sussarellu <fabio@Big-Chameleon.local>
2019-12-03 15:13:34 +0100

    load zsh syntax

--- macOS.sh
+++ macOS.sh
@@ -3,6 +3,9 @@
 # zsh-completions
 fpath=(/usr/local/share/zsh-completions $fpath)
 
+# syntax highlighting
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
 # pure loading
 source $bashthingsModules/pure.sh
 prompt pure

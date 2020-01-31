58e7448432fbfc0fa1061cda85187298c7bd9e83
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-01-31 17:58:21 +0100

    ADD -l for lolcat

--- modules/alias.sh
+++ modules/alias.sh
@@ -4,5 +4,5 @@ alias yy="yay -Syyu --noconfirm --sudoloop"
 alias suspend="systemctl suspend"
 alias webhook=$bashthingsScripts/webhook/webhook.sh
 alias bash-things-update=$bashthingsScripts/bash-things-update.sh
-alias cat=lolcat
+alias cat=lolcat -l
 alias sucode="sudo code --user-data-dir=\"${HOME}/.vscode-root\""

836cac4d38dca806a58ad63337a4ace08c206bc5
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-01-31 13:12:35 +0100

    ADD completion for webhook

--- loader.sh
+++ loader.sh
@@ -12,6 +12,9 @@ source $bashthingsModules/docker.sh
 # custom alias
 source $bashthingsModules/alias.sh
 
+# webhook completion
+source $bashthingsScripts/webhook/webhook-completion.sh
+
 # use nano
 export EDITOR=nano
 

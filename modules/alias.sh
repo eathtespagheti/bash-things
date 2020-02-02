2b0633588877c85919167098908571f99b08819d
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-02-02 23:25:59 +0100

    ADD tempfix for totem

--- modules/alias.sh
+++ modules/alias.sh
@@ -6,3 +6,4 @@ alias webhook=$bashthingsScripts/webhook/webhook.sh
 alias bash-things-update=$bashthingsScripts/bash-things-update.sh
 alias cat="lolcat -l"
 alias sucode="sudo code --user-data-dir=\"${HOME}/.vscode-root\""
+alias fixtotem="allow_rgb10_configs=false totem"

fa9e015439139d84343bdc10da72089323c58c45
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-05 12:03:29 +0100

    improve git aliases

--- firstrun/git.sh
+++ firstrun/git.sh
@@ -3,3 +3,7 @@
 bgecho '--- Enabling GIT superpush ---'
 git config --global alias.ultrapush '!git remote | xargs -L1 git push --all'
 git config --global alias.superpush '!git remote | xargs -L1 git push'
+
+bgecho '--- Enabling recursive clone ---'
+git config --global alias.superclone 'clone --recurse-submodules'
+git config --global alias.supermodule 'submodule add --recurse-submodules'

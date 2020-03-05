4f6659af312535d0cf19ee3cb44421f74b4929d7
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-05 12:16:45 +0100

    UPDATE recursive clone aliases

--- firstrun/git.sh
+++ firstrun/git.sh
@@ -5,5 +5,6 @@ git config --global alias.ultrapush '!git remote | xargs -L1 git push --all'
 git config --global alias.superpush '!git remote | xargs -L1 git push'
 
 bgecho '--- Enabling recursive clone ---'
-git config --global alias.superclone 'clone --recurse-submodules'
-git config --global alias.supermodule 'submodule add --recurse-submodules'
+git config --global alias.cloner 'clone --recurse-submodules'
+git config --global alias.updater 'submodule update --init --recursive'
+git config --global alias.subadd 'submodule add'

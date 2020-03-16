5abda6db06a72f9415780c2088ac9ddc632cf10f
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-16 12:05:53 +0100

    ADD JAVA_HOME

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -4,3 +4,4 @@ export bashthingsFolder=$HOME/.bash_things
 export bashthingsFunctions=$bashthingsFolder/functions
 export bashthingsScripts=$bashthingsFolder/scripts
 export bashthingsModules=$bashthingsFolder/modules
+export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")

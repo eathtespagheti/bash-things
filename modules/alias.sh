9d1e6c51a9253666ee7814eecd52275c1daba88c
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-25 09:07:45 +0100

    UPDATE yy alias

--- modules/alias.sh
+++ modules/alias.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-alias yy="yay -Syyu --noconfirm --sudoloop"
+alias yy="yay -Syu --noconfirm --sudoloop"
 alias suspend="systemctl suspend"
 alias webhook=$bashthingsScripts/webhook/webhook.sh
 alias bash-things-update=$bashthingsScripts/bash-things-update.sh

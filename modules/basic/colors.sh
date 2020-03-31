b1800e3c4891c547efeda5f18a940b3c630274fa
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-31 23:25:09 +0200

    improve colors declaration

--- modules/basic/colors.sh
+++ modules/basic/colors.sh
@@ -1,99 +1,90 @@
-#!/usr/bin/env bash
-
-#######################################################################################################
-
-#  Customize BASH PS1 prompt to show current GIT repository and branch.
-#  by Mike Stewart - http://MediaDoneRight.com
-
-#  SETUP CONSTANTS
-#  Bunch-o-predefined colors.  Makes reading code easier than escape sequences.
-#  I don't remember where I found this.  o_O
+#!/usr/bin/env sh
 
 # Reset
-Color_Off="\[\033[0m\]"       # Text Reset
+export Color_Off="\[\033[0m\]" # Text Reset
 
 # Regular Colors
-Black="\[\033[0;30m\]"        # Black
-Red="\[\033[0;31m\]"          # Red
-Green="\[\033[0;32m\]"        # Green
-Yellow="\[\033[0;33m\]"       # Yellow
-Blue="\[\033[0;34m\]"         # Blue
-Purple="\[\033[0;35m\]"       # Purple
-Cyan="\[\033[0;36m\]"         # Cyan
-White="\[\033[0;37m\]"        # White
+export Black="\[\033[0;30m\]"  # Black
+export Red="\[\033[0;31m\]"    # Red
+export Green="\[\033[0;32m\]"  # Green
+export Yellow="\[\033[0;33m\]" # Yellow
+export Blue="\[\033[0;34m\]"   # Blue
+export Purple="\[\033[0;35m\]" # Purple
+export Cyan="\[\033[0;36m\]"   # Cyan
+export White="\[\033[0;37m\]"  # White
 
 # Bold
-BBlack="\[\033[1;30m\]"       # Black
-BRed="\[\033[1;31m\]"         # Red
-BGreen="\[\033[1;32m\]"       # Green
-BYellow="\[\033[1;33m\]"      # Yellow
-BBlue="\[\033[1;34m\]"        # Blue
-BPurple="\[\033[1;35m\]"      # Purple
-BCyan="\[\033[1;36m\]"        # Cyan
-BWhite="\[\033[1;37m\]"       # White
+export BBlack="\[\033[1;30m\]"  # Black
+export BRed="\[\033[1;31m\]"    # Red
+export BGreen="\[\033[1;32m\]"  # Green
+export BYellow="\[\033[1;33m\]" # Yellow
+export BBlue="\[\033[1;34m\]"   # Blue
+export BPurple="\[\033[1;35m\]" # Purple
+export BCyan="\[\033[1;36m\]"   # Cyan
+export BWhite="\[\033[1;37m\]"  # White
 
 # Underline
-UBlack="\[\033[4;30m\]"       # Black
-URed="\[\033[4;31m\]"         # Red
-UGreen="\[\033[4;32m\]"       # Green
-UYellow="\[\033[4;33m\]"      # Yellow
-UBlue="\[\033[4;34m\]"        # Blue
-UPurple="\[\033[4;35m\]"      # Purple
-UCyan="\[\033[4;36m\]"        # Cyan
-UWhite="\[\033[4;37m\]"       # White
+export UBlack="\[\033[4;30m\]"  # Black
+export URed="\[\033[4;31m\]"    # Red
+export UGreen="\[\033[4;32m\]"  # Green
+export UYellow="\[\033[4;33m\]" # Yellow
+export UBlue="\[\033[4;34m\]"   # Blue
+export UPurple="\[\033[4;35m\]" # Purple
+export UCyan="\[\033[4;36m\]"   # Cyan
+export UWhite="\[\033[4;37m\]"  # White
 
 # Background
-On_Black="\[\033[40m\]"       # Black
-On_Red="\[\033[41m\]"         # Red
-On_Green="\[\033[42m\]"       # Green
-On_Yellow="\[\033[43m\]"      # Yellow
-On_Blue="\[\033[44m\]"        # Blue
-On_Purple="\[\033[45m\]"      # Purple
-On_Cyan="\[\033[46m\]"        # Cyan
-On_White="\[\033[47m\]"       # White
+export On_Black="\[\033[40m\]"  # Black
+export On_Red="\[\033[41m\]"    # Red
+export On_Green="\[\033[42m\]"  # Green
+export On_Yellow="\[\033[43m\]" # Yellow
+export On_Blue="\[\033[44m\]"   # Blue
+export On_Purple="\[\033[45m\]" # Purple
+export On_Cyan="\[\033[46m\]"   # Cyan
+export On_White="\[\033[47m\]"  # White
 
 # High Intensty
-IBlack="\[\033[0;90m\]"       # Black
-IRed="\[\033[0;91m\]"         # Red
-IGreen="\[\033[0;92m\]"       # Green
-IYellow="\[\033[0;93m\]"      # Yellow
-IBlue="\[\033[0;94m\]"        # Blue
-IPurple="\[\033[0;95m\]"      # Purple
-ICyan="\[\033[0;96m\]"        # Cyan
-IWhite="\[\033[0;97m\]"       # White
+export IBlack="\[\033[0;90m\]"  # Black
+export IRed="\[\033[0;91m\]"    # Red
+export IGreen="\[\033[0;92m\]"  # Green
+export IYellow="\[\033[0;93m\]" # Yellow
+export IBlue="\[\033[0;94m\]"   # Blue
+export IPurple="\[\033[0;95m\]" # Purple
+export ICyan="\[\033[0;96m\]"   # Cyan
+export IWhite="\[\033[0;97m\]"  # White
 
 # Bold High Intensty
-BIBlack="\[\033[1;90m\]"      # Black
-BIRed="\[\033[1;91m\]"        # Red
-BIGreen="\[\033[1;92m\]"      # Green
-BIYellow="\[\033[1;93m\]"     # Yellow
-BIBlue="\[\033[1;94m\]"       # Blue
-BIPurple="\[\033[1;95m\]"     # Purple
-BICyan="\[\033[1;96m\]"       # Cyan
-BIWhite="\[\033[1;97m\]"      # White
+export BIBlack="\[\033[1;90m\]"  # Black
+export BIRed="\[\033[1;91m\]"    # Red
+export BIGreen="\[\033[1;92m\]"  # Green
+export BIYellow="\[\033[1;93m\]" # Yellow
+export BIBlue="\[\033[1;94m\]"   # Blue
+export BIPurple="\[\033[1;95m\]" # Purple
+export BICyan="\[\033[1;96m\]"   # Cyan
+export BIWhite="\[\033[1;97m\]"  # White
 
 # High Intensty backgrounds
-On_IBlack="\[\033[0;100m\]"   # Black
-On_IRed="\[\033[0;101m\]"     # Red
-On_IGreen="\[\033[0;102m\]"   # Green
-On_IYellow="\[\033[0;103m\]"  # Yellow
-On_IBlue="\[\033[0;104m\]"    # Blue
-On_IPurple="\[\033[10;95m\]"  # Purple
-On_ICyan="\[\033[0;106m\]"    # Cyan
-On_IWhite="\[\033[0;107m\]"   # White
+export On_IBlack="\[\033[0;100m\]"  # Black
+export On_IRed="\[\033[0;101m\]"    # Red
+export On_IGreen="\[\033[0;102m\]"  # Green
+export On_IYellow="\[\033[0;103m\]" # Yellow
+export On_IBlue="\[\033[0;104m\]"   # Blue
+export On_IPurple="\[\033[10;95m\]" # Purple
+export On_ICyan="\[\033[0;106m\]"   # Cyan
+export On_IWhite="\[\033[0;107m\]"  # White
 
 # Various variables you might want for your PS1 prompt instead
-Time12h="\T"
-Time12a="\@"
-PathShort="\W"
-PathFull="\w"
-NewLine="\n"
-Jobs="\j"
-User="\u"
-Host="\h"
+export Time12h="\T"
+export Time12a="\@"
+export PathShort="\W"
+export PathFull="\w"
+export NewLine="\n"
+export Jobs="\j"
+export User="\u"
+export Host="\h"
 
-LightNewCyan="\[\033[1;96m\]"
-NewCyan="\[\033[0;96m\]"
-NewBlue="\[\033[0;94m\]"
-LightNewBlue="\[\033[1;94m\]"
-LightPurple="\[\033[1;95m\]"
+export LightNewCyan="\[\033[1;96m\]"
+export NewCyan="\[\033[0;96m\]"
+export NewBlue="\[\033[0;94m\]"
+export LightNewBlue="\[\033[1;94m\]"
+export LightPurple="\[\033[1;95m\]"

d88a143afe22ed1d5e6e563299dd4a1bc204ec09
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-31 23:27:01 +0200

    ADD shellcheck comments

--- linux.sh
+++ linux.sh
@@ -4,6 +4,7 @@
 [ "$(command -v loadkeys)" ] && loadkeys it
 
 # webhook completion
+# shellcheck disable=SC1090
 [ -d "$HOME/.webhook" ] && source "$BASHTHINGS_SCRIPTS/webhook/webhook-completion.sh"
 
 # enable globstar
@@ -13,11 +14,15 @@ shopt -s globstar
 # export MAKEFLAGS="${MAKEFLAGS} -j$(lscpu -b -p=CPU | grep -v '^#' | sort -u | wc -l)"
 
 # load easymount
+# shellcheck disable=SC1090
 source "$BASHTHINGS_FUNCTIONS/easymount.sh"
 
 # BigChameleon specific configuration
+# shellcheck disable=SC1090
 [ "$(uname -n)" = "BigChameleon" ] && source "$BASHTHINGS_MODULES/bigChameleon.sh"
 
 # trueline settings
+# shellcheck disable=SC1090
 source "$BASHTHINGS_MODULES/trueline-settings.sh"
+# shellcheck disable=SC1090
 source "$BASHTHINGS_MODULES/trueline/trueline.sh"

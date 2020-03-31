d2447f47e6b48fdd645d23b370f83510adceaca0
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-31 23:21:58 +0200

    FIX syntax

--- modules/basic/bash_string.sh
+++ modules/basic/bash_string.sh
@@ -3,10 +3,9 @@
 # custom .bashrc
 # get current branch in git repo
 function parse_git_branch() {
-  BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
-  if [ ! "${BRANCH}" == "" ]
-  then
-    STAT=`parse_git_dirty`
+  BRANCH=$(git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+  if [ ! "${BRANCH}" == "" ]; then
+    STAT=$(parse_git_dirty)
     echo "${BRANCH}${STAT} "
   else
     echo ""
@@ -14,14 +13,32 @@ function parse_git_branch() {
 }
 
 # get current status of git repo
-function parse_git_dirty {
-  status=`git status 2>&1 | tee`
-  dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
-  untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
-  ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
-  newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
-  renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
-  deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
+function parse_git_dirty() {
+  status=$(git status 2>&1 | tee)
+  dirty=$(
+    echo -n "${status}" 2>/dev/null | grep "modified:" &>/dev/null
+    echo "$?"
+  )
+  untracked=$(
+    echo -n "${status}" 2>/dev/null | grep "Untracked files" &>/dev/null
+    echo "$?"
+  )
+  ahead=$(
+    echo -n "${status}" 2>/dev/null | grep "Your branch is ahead of" &>/dev/null
+    echo "$?"
+  )
+  newfile=$(
+    echo -n "${status}" 2>/dev/null | grep "new file:" &>/dev/null
+    echo "$?"
+  )
+  renamed=$(
+    echo -n "${status}" 2>/dev/null | grep "renamed:" &>/dev/null
+    echo "$?"
+  )
+  deleted=$(
+    echo -n "${status}" 2>/dev/null | grep "deleted:" &>/dev/null
+    echo "$?"
+  )
   bits=''
   if [ "${renamed}" == "0" ]; then
     bits=">${bits}"
@@ -49,20 +66,22 @@ function parse_git_dirty {
 }
 
 # get active docker machine
-function parse_docker_machine {
-  if [ "$MACHINE" = "" ]
-  then echo ""
+function parse_docker_machine() {
+  if [ "$MACHINE" = "" ]; then
+    echo ""
   else
     echo "| ${MACHINE} "
   fi
 }
 
-function apple_status {
-  if [ "$?" -eq "0" ]
-  then APPLE="👽"
-  else APPLE="✝"
+function apple_status() {
+  # shellcheck disable=SC2181
+  if [ "$?" -eq "0" ]; then
+    EMOJI="👽"
+  else
+    EMOJI="✝"
   fi
-  echo $APPLE
+  echo $EMOJI
 }
 
 # docker-machine PS1

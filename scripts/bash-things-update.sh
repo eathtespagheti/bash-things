35fcb91d954af595165ac603f5160be77fc8040e
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-31 23:25:52 +0200

    update for sh

--- scripts/bash-things-update.sh
+++ scripts/bash-things-update.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 gecho "Updating bash-things"
-git -C $BASHTHINGS_FOLDER pull
+git -C "$BASHTHINGS_FOLDER" pull
 echo
 
 gecho "Last 5 commits were"
-git -C $BASHTHINGS_FOLDER log -n 5 --format=oneline
+git -C "$BASHTHINGS_FOLDER" log -n 5 --format=oneline
 echo
 
 gecho "Update submodules"
-git -C $BASHTHINGS_FOLDER submodule update --init --recursive
+git -C "$BASHTHINGS_FOLDER" submodule update --init --recursive
 
 # gecho "Updating trueline"
 # git -C $BASHTHINGS_MODULES/trueline pull

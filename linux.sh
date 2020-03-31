93cf509d5d6a9ea7ae4415e1c2e65cb2b3ae15d6
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-31 23:18:22 +0200

    improve linux.sh syntax

--- linux.sh
+++ linux.sh
@@ -4,7 +4,7 @@
 [ "$(command -v loadkeys)" ] && loadkeys it
 
 # webhook completion
-[ -d "$HOME/.webhook" ] && source "$BASHTHINGS_SCRIPTS"/webhook/webhook-completion.sh
+[ -d "$HOME/.webhook" ] && source "$BASHTHINGS_SCRIPTS/webhook/webhook-completion.sh"
 
 # enable globstar
 shopt -s globstar
@@ -13,11 +13,11 @@ shopt -s globstar
 # export MAKEFLAGS="${MAKEFLAGS} -j$(lscpu -b -p=CPU | grep -v '^#' | sort -u | wc -l)"
 
 # load easymount
-source "$BASHTHINGS_FUNCTIONS"/easymount.sh
+source "$BASHTHINGS_FUNCTIONS/easymount.sh"
 
 # BigChameleon specific configuration
-[ "$(uname -n)" = "BigChameleon" ] && source "$BASHTHINGS_MODULES"/bigChameleon.sh
+[ "$(uname -n)" = "BigChameleon" ] && source "$BASHTHINGS_MODULES/bigChameleon.sh"
 
 # trueline settings
-source $BASHTHINGS_MODULES/trueline-settings.sh
-source $BASHTHINGS_MODULES/trueline/trueline.sh
+source "$BASHTHINGS_MODULES/trueline-settings.sh"
+source "$BASHTHINGS_MODULES/trueline/trueline.sh"

09dfe268e7ec5ba66b5b87ad7a135368addb7c06
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-04-02 15:16:58 +0200

    clean alias

--- modules/alias.sh
+++ modules/alias.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env sh
 
+# System management
 alias yy="yay -Syu --noconfirm --sudoloop"
 alias suspend="systemctl suspend"
-alias webhook='$BASHTHINGS_SCRIPTS/webhook/webhook.sh'
-alias bash-things-update='$BASHTHINGS_SCRIPTS/bash-things-update.bash'
+
+# Software I use
 [ "$(command -v code)" ] && code='code'
 [ "$(command -v code-insiders)" ] && code='code-insiders'
 [ -n "$code" ] && alias sucode='$code --user-data-dir $XDG_CONFIG_HOME/vscode-root'
-[ "$(command -v lolcat)" ] && alias cat='lolcat -l'
 [ -n "$EDITOR" ] && alias edit='$EDITOR' && alias suedit='sudo $EDITOR'
 [ -n "$EDITOR_CONSOLE" ] && alias editc='$EDITOR_CONSOLE' && alias sueditc='sudo $EDITOR_CONSOLE'
-# alias fixtotem="allow_rgb10_configs=false totem"
+
+# Wrappers
+alias webhook='$BASHTHINGS_SCRIPTS/webhook/webhook.sh'
 alias cps="rsync -ah --progress"
+[ "$(command -v lolcat)" ] && alias cat='lolcat -l' && alias ocat='cat'
+alias ccat='highlight --out-format=ansi'
+
+# Bash things specifics
+alias bash-things-update='$BASHTHINGS_SCRIPTS/bash-things-update.bash'
 alias editBashThings='$EDITOR $BASHTHINGS_FOLDER'
-alias loadNVM="source /usr/share/nvm/init-nvm.sh"
 alias loadVirtualenvwrapper='source $BASHTHINGS_MODULES/virtualenvwrapper.sh'
+alias loadNVM="source /usr/share/nvm/init-nvm.sh"

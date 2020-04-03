77add658089801aa7ee3841fbbeb0e4c6f3cab85
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-04-03 10:34:22 +0200

    IMPROVE XDG paths for load_themes

--- scripts/load_themes.sh
+++ scripts/load_themes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # shellcheck disable=SC2155
 
-[ -z "$LS_THEMES_FOLDER" ] && LS_THEMES_FOLDER="$XDG_CONFIG_HOME"/ls_themes/themes
+[ -z "$LS_THEMES_FOLDER" ] && LS_THEMES_FOLDER="$XDG_CONFIG_HOME"/load_themes
 [ -z "$1" ] && theme="$LS_THEME" || theme="$1"
 [ -n "$theme" ] && export LS_COLORS="$(cat "$LS_THEMES_FOLDER"/"$theme")"

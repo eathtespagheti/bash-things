4db6dc66585274dbe3c5b5be0a508899619eaa54
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-04-03 10:30:57 +0200

    ADD XDG_CONFIG_HOME to load_themes

--- scripts/ls_themes/load_themes.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-# shellcheck disable=SC2155
-
-themesFolder="$BASHTHINGS_SCRIPTS"/ls_themes/themes
-[ -z "$1" ] && theme="$LS_THEME" || theme="$1"
-[ -n "$theme" ] && export LS_COLORS="$(cat "$themesFolder"/"$theme")"

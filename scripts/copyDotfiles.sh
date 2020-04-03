b3cad643aa0f363e008e5540129b6d71de2b6a76
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-04-03 10:52:32 +0200

    improve copyDotfiles

--- scripts/copyDotfiles.sh
+++ scripts/copyDotfiles.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+# shellcheck disable=SC2015
 
 dotfilesFolder="$BASHTHINGS_FOLDER"/dotfiles/.config
 outputFolder="$XDG_CONFIG_HOME"
@@ -13,13 +14,30 @@ makeBackup() {
         outputFile="$1.$count.backup"
         count=$((count + 1))
     done
-    mv "$1" "$outputFile" 
+    mv "$1" "$outputFile"
 }
+
+# delete a single backup file
+delete() {
+    rm -rf "$1"
+}
+
+# Delete all backups from folder
+deleteBackups() {
+    # backups="$(ls "$outputFolder"/*.backup)"
+    for backup in "$outputFolder"/*.backup; do
+        delete "$backup"
+    done
+}
+
+[ "$1" = "-h" ] && echo "Run with -n to disable backups, run with -d to delete all backups file in folder at the end, run with -h to show this message" && exit 0
 
 for dotfile in $dotfiles; do
     outfile="$outputFolder"/"$dotfile"
     # If dotfile alreaxy exist in destination make a backup
-    [ -d "$outfile" ] || [ -f "$outfile" ] && makeBackup "$outfile"
+    [ -d "$outfile" ] || [ -f "$outfile" ] && ([ "$1" = "-n" ] && delete "$outfile" || makeBackup "$outfile")
     # Make a link with the original folder
     ln -s "$dotfilesFolder"/"$dotfile" "$outputFolder"/"$dotfile"
 done
+
+[ "$1" = "-d" ] && deleteBackups || exit 0

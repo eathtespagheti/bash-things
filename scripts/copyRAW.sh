99d6a5e527063c70e12aa677e618ec155fbebfc4
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-04-07 21:36:01 +0200

    ADD copyraw script

---
+++ scripts/copyRAW.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Check default variables
+[ -z "$COPYRAW_BASEDIR" ] && COPYRAW_BASEDIR="/run/media/$USER"
+[ -z "$COPYRAW_DCIM_FOLDER" ] && COPYRAW_DCIM_FOLDER="DCIM"
+[ -z "$RAW_PICTURES_FOLDER" ] && RAW_PICTURES_FOLDER="$HOME/Pictures/DCIM/RAW"
+
+# Declare source and destination folder
+source="$COPYRAW_BASEDIR/$1/$COPYRAW_DCIM_FOLDER"
+destination="$RAW_PICTURES_FOLDER/$1"
+
+# Check if source DCIM folder exist
+[ ! -d "$source" ] && echo "Unable to find $source directory" && exit 1
+
+# Check if folder exist, if not create it
+[ ! -d "$destination" ] && mkdir -p "$destination"
+
+# Run rsync
+rsync -amruv --include='*.nef' --include='*.NEF' --include='*.ARW' --include='*.arw' --include='*/' --exclude='*' "$source" "$destination"

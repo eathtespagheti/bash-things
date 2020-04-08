35c1f2ce72be2b2b45c0fce7c06085301f18d45d
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-04-08 12:16:44 +0200

    improve completions

--- scripts/completions/copyRAW-completion.bash
+++ scripts/completions/copyRAW-completion.bash
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+# If default dir doesn't exist
 [ -z "$COPYRAW_BASEDIR" ] && export COPYRAW_BASEDIR="/run/media/$USER"
+
 _copyRAW_completions() {
     # No other args
     if [ "${#COMP_WORDS[@]}" -gt "2" ]; then
         return
     fi
 
+    # Check if directory exist
     [ ! -d "$COPYRAW_BASEDIR" ] && return
     mapfile -t COMPREPLY < <(compgen -W "$(ls "$COPYRAW_BASEDIR")" -- "${COMP_WORDS[1]}")
 }
--- scripts/completions/linkToBin-completion.bash
+++ scripts/completions/linkToBin-completion.bash
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2045
 
-files=""
-for item in $(ls "$BASHTHINGS_SCRIPTS"); do
-    [ -f "$BASHTHINGS_SCRIPTS/$item" ] && files="$files $item"
-done
+_linkToBin_completions() {
+    # No other args
+    if [ "${#COMP_WORDS[@]}" -gt "2" ]; then
+        return
+    fi
 
-[ -n "$BASHTHINGS_SCRIPTS" ] && complete -W "$files" linkToBin
+    # Check if directory exist
+    [ ! -d "$BASHTHINGS_SCRIPTS" ] && return
+
+    # Generate files list
+    files=""
+    for item in $(ls "$BASHTHINGS_SCRIPTS"); do
+        [ -f "$BASHTHINGS_SCRIPTS/$item" ] && files="$files $item"
+    done
+
+    mapfile -t COMPREPLY < <(compgen -W "$files" -- "${COMP_WORDS[1]}")
+}
+
+complete -F _linkToBin_completions linkToBin

dad25576219a02f318f4cfb1a155f2f06f7de654
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-04-08 11:59:18 +0200

    improve copyraw completion

--- scripts/completions/copyRAW-completion.bash
+++ scripts/completions/copyRAW-completion.bash
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-[ -z "$COPYRAW_BASEDIR" ] && COPYRAW_BASEDIR="/run/media/$USER"
-complete -W "$(ls "$COPYRAW_BASEDIR")" copyRAW # Run command for autocomplete
+[ -z "$COPYRAW_BASEDIR" ] && export COPYRAW_BASEDIR="/run/media/$USER"
+_copyRAW_completions() {
+    if [ "${#COMP_WORDS[@]}" != "2" ]; then
+        return
+    fi
+
+    [ ! -d "$COPYRAW_BASEDIR" ] && return
+    mapfile -t COMPREPLY < <(compgen -W "$(ls "$COPYRAW_BASEDIR")" -- "${COMP_WORDS[1]}")
+}
+
+complete -F _copyRAW_completions copyRAW

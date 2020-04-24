888f9cae3958dba01e070ddcefd11a23affc478d
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-04-25 01:52:53 +0200

    improve mesa installing in firstrun

--- firstrun/Arch/mesa.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo
-bgecho '--- MESA INSTALL ---'
-
-gecho 'Vulkan libraries, Mesa Drivers, DXVK and Lutris'
-$PACKAGE_INSTALL vulkan-radeon lib32-vulkan-radeon lib32-vulkan-icd-loader mesa $AUTOYES
-echo 'OK'
--- firstrun/firstrun-arch.sh
+++ firstrun/firstrun-arch.sh
@@ -1,164 +1,205 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+# shellcheck disable=SC1090
 
-# Loading variables
-source $HOME/.bash_things/scripts/variables.sh
+# Loading variables and XDG_PATHS
+. "$BASHTHINGS_FOLDER/modules/variables.sh"
+. "$BASHTHINGS_FOLDER/modules/XDG_PATHS.sh"
+# Add firstrun bin to path
+export PATH="$PATH:$BASHTHINGS_FOLDER/firstrun/bin"
+
 DISTRO='Arch'
-bashthingsFirstrun=$BASHTHINGS_FOLDER/firstrun
-bashthingsFirstrunSpecific=$bashthingsFirstrun/$DISTRO
-export AUTOYES='--noconfirm'
-export PACKAGE_INSTALL='yay -S'
-ALLTRUE=false
+bashthingsFirstrun="$BASHTHINGS_FOLDER/firstrun"
+bashthingsFirstrunSpecific="$bashthingsFirstrun/$DISTRO"
+export PACKAGE_CHECK="pacman -Q"
+export PACKAGE_INSTALL="pacman -S"
+export AUTOYES="--noconfirm"
+ALLTRUE="false"
 
-# Loading echo functions
-source $BASHTHINGS_FUNCTIONS/echo.sh
-
-if [ "$1" = "--alltrue" ]; then
-  ALLTRUE=true
-fi
+[ "$1" = "--alltrue" ] && ALLTRUE="true"
 
 # Package update and upgrade
 gecho 'Updating and upgrading packages'
-sudo pacman -Syyu $AUTOYES
+# TODO: UNCOMMENT THIS LINE
+# sudo pacman -Syu $AUTOYES
 
-gecho 'Install yay'
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si $AUTOYES
-cd ..
-rm -rf yay
+# If yay it's not installed
+[ ! "$(command -v yay)" ] && {
+  gecho 'Install yay'
+  git clone https://aur.archlinux.org/yay.git
+  cd yay || {
+    echo "Error! yay folder not found! Exiting..."
+    exit 1
+  }
+  makepkg -si $AUTOYES
+  cd ..
+  rm -rf yay
+}
+# Update autoyes value
+export PACKAGE_CHECK="yay -Q"
+export PACKAGE_INSTALL="yay -S"
+export AUTOYES="$AUTOYES --sudoloop"
 
-if [[ $ALLTRUE == true ]]; then
+# Copy dotfiles
+echo
+gecho "Copy dotfiles"
+copyDotfiles --replace-backups
+echo
+
+if [ "$ALLTRUE" = "true" ]; then
   # Enable bash_things
-  bgecho 'Enable bash_things in .bashrc'
-  echo >>$HOME/.bashrc
-  echo "# Customization" >>$HOME/.bashrc
-  echo "source ${BASHTHINGS_FOLDER}/loader.sh" >>$HOME/.bashrc
+  # If the bashthings file isn't sourced
+  grep -q "[^#] *\$BASHTHINGS_FOLDER\/loader.sh" "$HOME/.bashrc" || {
+    bgecho 'Enable bash_things in .bashrc'
+    {
+      echo
+      echo "# Customization"
+      echo "source ${BASHTHINGS_FOLDER}/loader.sh"
+    } >>"$HOME/.bashrc"
+  }
 
   # Case insensitive autocomplete
-  $bashthingsFirstrun/case-insensitive-autocomplete.sh
+  "$bashthingsFirstrun/case-insensitive-autocomplete.sh"
   # Improved Graphic Drivers
-  $bashthingsFirstrunSpecific/mesa.sh
+  customInstaller vulkan-radeon lib32-vulkan-radeon lib32-vulkan-icd-loader mesa
   # Dev-Tools
-  $bashthingsFirstrunSpecific/dev-tools.sh
+  "$bashthingsFirstrunSpecific/dev-tools.sh"
   # Broadcom WiFi
-  $bashthingsFirstrunSpecific/broadcom.sh
+  "$bashthingsFirstrunSpecific/broadcom.sh"
   # BMC43142 Bluetooth fix
-  $bashthingsFirstrun/BMC43142.sh
+  "$bashthingsFirstrun/BMC43142.sh"
   # Docker
-  $bashthingsFirstrunSpecific/docker.sh
+  "$bashthingsFirstrunSpecific/docker.sh"
   # docker-machine
-  $bashthingsFirstrunSpecific/docker-machine.sh
+  "$bashthingsFirstrunSpecific/docker-machine.sh"
   # NVM
-  $bashthingsFirstrunSpecific/nvm.sh
+  "$bashthingsFirstrunSpecific/nvm.sh"
   # GIT superpush
-  $bashthingsFirstrun/git.sh
+  "$bashthingsFirstrun/git.sh"
   # screen-sleep
-  $bashthingsFirstrunSpecific/screen-sleep.sh
+  "$bashthingsFirstrunSpecific/screen-sleep.sh"
   # lolcat
-  $bashthingsFirstrunSpecific/lolcat.sh
+  "$bashthingsFirstrunSpecific/lolcat.sh"
 else
   # Enable bash_things
-  read -p "Enable bash_things? [Y/n] " -n 1
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Enable bash_things? [Y/n] "
+  read -r REPLY
+  if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
     echo
   else
     # Enable bash_things
-    bgecho 'Enable bash_things in .bashrc'
-    echo >>$HOME/.bashrc
-    echo "# Customization" >>$HOME/.bashrc
-    echo "source ${BASHTHINGS_FOLDER}/loader.sh" >>$HOME/.bashrc
+    # If the bashthings file isn't sourced
+    grep -q "[^#] *\$BASHTHINGS_FOLDER\/loader.sh" "$HOME/.bashrc" || {
+      bgecho 'Enable bash_things in .bashrc'
+      {
+        echo
+        echo "# Customization"
+        echo "source ${BASHTHINGS_FOLDER}/loader.sh"
+      } >>"$HOME/.bashrc"
+    }
   fi
 
   # Case insensitive autocomplete
-  read -p "Enable case insensitive autocomplete? [Y/n] " -n 1
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Enable case insensitive autocomplete? [Y/n] "
+  read -r REPLY
+  if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
     echo
   else
-    $bashthingsFirstrun/case-insensitive-autocomplete.sh
+    "$bashthingsFirstrun/case-insensitive-autocomplete.sh"
   fi
 
   # Improved Graphic Drivers
-  read -p "Enable Improved Graphic Drivers? [Y/n] " -n 1
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Enable Improved Graphic Drivers? [Y/n] "
+  read -r REPLY
+  if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
     echo
   else
-    $bashthingsFirstrunSpecific/mesa.sh
+    # TODO: Script for enabling 32bit packages in pacman
+    customInstaller vulkan-radeon lib32-vulkan-radeon lib32-vulkan-icd-loader mesa
   fi
 
   # Dev-Tools
-  read -p "Install Dev-Tools? [Y/n] " -n 1
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Install Dev-Tools? [Y/n] "
+  read -r REPLY
+  if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
     echo
   else
-    $bashthingsFirstrunSpecific/dev-tools.sh
+    "$bashthingsFirstrunSpecific/dev-tools.sh"
   fi
 
   # Broadcom WiFi
-  read -p "Install Broadcom WiFi Drivers? [Y/n] " -n 1
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Install Broadcom WiFi Drivers? [Y/n] "
+  read -r REPLY
+  if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
     echo
   else
-    $bashthingsFirstrunSpecific/broadcom.sh
+    "$bashthingsFirstrunSpecific/broadcom.sh"
   fi
 
   # BMC43142 Bluetooth fix
-  read -p "Fix Broadcom BMC43142 Bluetooth? [Y/n] " -n 1
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Fix Broadcom BMC43142 Bluetooth? [Y/n] "
+  read -r REPLY
+  if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
     echo
   else
-    $bashthingsFirstrun/BMC43142.sh
+    "$bashthingsFirstrun/BMC43142.sh"
   fi
 
   # Docker
-  read -p "Install Docker? [Y/n] " -n 1
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Install Docker? [Y/n] "
+  read -r REPLY
+  if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
     echo
   else
-    $bashthingsFirstrunSpecific/docker.sh
+    "$bashthingsFirstrunSpecific/docker.sh"
   fi
 
   # docker-machine
-  read -p "Install docker-machine? [Y/n] " -n 1
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Install docker-machine? [Y/n] "
+  read -r REPLY
+  if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
     echo
   else
-    $bashthingsFirstrunSpecific/docker-machine.sh
+    "$bashthingsFirstrunSpecific/docker-machine.sh"
   fi
 
   # NVM
-  read -p "Install NVM? [Y/n] " -n 1
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Install NVM? [Y/n] "
+  read -r REPLY
+  if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
     echo
   else
-    $bashthingsFirstrunSpecific/nvm.sh
+    "$bashthingsFirstrunSpecific/nvm.sh"
   fi
 
   # GIT superpush
-  read -p "Enable GIT superpush? [Y/n] " -n 1
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Enable GIT superpush? [Y/n] "
+  read -r REPLY
+  if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
     echo
   else
-    $bashthingsFirstrun/git.sh
+    "$bashthingsFirstrun/git.sh"
   fi
 
   # screen-sleep
-  read -p "Install screen-sleep? [Y/n] " -n 1
-  if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "Install screen-sleep? [Y/n] "
+  read -r REPLY
+  if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
     echo
   else
-    $bashthingsFirstrunSpecific/screen-sleep.sh
+    "$bashthingsFirstrunSpecific/screen-sleep.sh"
   fi
 
   # lolcat
-  $bashthingsFirstrunSpecific/lolcat.sh
+  "$bashthingsFirstrunSpecific/lolcat.sh"
 fi
 
 # echo Done
 bgecho 'Done!'
 
 # Final reboot
-read -p "Reboot system now? [Y/n] " -n 1
-if [[ $REPLY =~ ^[Nn]$ ]]; then
+echo "Reboot system now? [Y/n] "
+read -r REPLY
+if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
   echo
 else
   sudo reboot

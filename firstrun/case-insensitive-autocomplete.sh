f784d63503cf5682e358e104514fc7ce0d444bc4
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-04-25 01:06:15 +0200

    fix case insensitive autocomplete script

--- firstrun/case-insensitive-autocomplete.sh
+++ firstrun/case-insensitive-autocomplete.sh
@@ -1,9 +1,16 @@
-# Case insensitive auto-complete
+#!/usr/bin/env sh
 
-bgecho 'Enabling case insensitive auto-complete...'
 # If ~./inputrc doesn't exist yet, first include the original /etc/inputrc so we don't override it
-if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
+[ ! -f "$INPUTRC" ] && {
+    directory="$(dirname "$INPUTRC")"
+    # If directory doesn't exist
+    [ ! -d "$directory" ] && mkdir -p "$directory"
+    # Create file
+    echo "\$include /etc/inputrc" >"$INPUTRC"
+}
 
 # Add option to ~/.inputrc to enable case-insensitive tab completion
-echo "set completion-ignore-case On" >> ~/.inputrc
-echo 'OK'
+grep -q "set completion-ignore-case On" "$INPUTRC" || {
+    bgecho 'Enabling case insensitive auto-complete...'
+    echo "set completion-ignore-case On" >>"$INPUTRC"
+}

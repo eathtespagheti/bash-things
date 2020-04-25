341a4d9b297fb8c09c06301b814d4eb38381f93f
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-04-25 10:55:07 +0200

    ADD installerprompt

---
+++ firstrun/installerPrompt.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+[ ! "$ALLTRUE" = "true" ] && {
+    [ -z "$PROMPT_MESSAGE" ] && PROMPT_MESSAGE="Execute \"$*\"?"
+    echo "$PROMPT_MESSAGE [Y/n]:"
+    read -r REPLY
+    if [ "$REPLY" = "N" ] || [ "$REPLY" = "n" ]; then
+        echo
+    else
+        "$@"
+    fi
+    exit
+}
+
+"$@"

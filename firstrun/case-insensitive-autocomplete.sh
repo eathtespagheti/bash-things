027f5569a481943dc31780a58b0ddce20048ec2c
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-04-25 10:55:45 +0200

    remove check not needed

--- firstrun/case-insensitive-autocomplete.sh
+++ firstrun/case-insensitive-autocomplete.sh
@@ -9,8 +9,5 @@
     echo "\$include /etc/inputrc" >"$INPUTRC"
 }
 
-# Add option to ~/.inputrc to enable case-insensitive tab completion
-grep -q "set completion-ignore-case On" "$INPUTRC" || {
-    bgecho 'Enabling case insensitive auto-complete...'
-    echo "set completion-ignore-case On" >>"$INPUTRC"
-}
+bgecho 'Enabling case insensitive auto-complete...'
+echo "set completion-ignore-case On" >>"$INPUTRC"

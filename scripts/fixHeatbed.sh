7ef665f7f310ecb44ba881bd8f03df8eda24e9d6
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-06-04 00:17:40 +0200

    Improve fixHeatbed

--- scripts/fixHeatbed.sh
+++ scripts/fixHeatbed.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
 
+fixDiffTemp="20"
 for parameter in "$@"; do
-    tr <"$parameter" '\n' '\r' | sed 's|;LAYER:1\rM140 S[0-9]*|;LAYER:1\rM140 S45|' | tr '\r' '\n' >"tmp$parameter" && mv "tmp$parameter" "$parameter"
+    numberOfLayers="$(grep "LAYER_COUNT" "$parameter")"
+    numberOfLayers="${numberOfLayers#;LAYER_COUNT:}"
+
+    originalTemperature="$(grep -m 1 "M140 S[0-9]*" "$parameter")"
+    originalTemperature="${originalTemperature#M140 S}"
+    fixTemp=$((originalTemperature - fixDiffTemp))
+
+    temperatureChangeLayer=$((numberOfLayers / 10))
+
+    tr <"$parameter" '\n' '\r' | sed "s|;LAYER:$temperatureChangeLayer\rM140 S[0-9]*|;LAYER:$temperatureChangeLayer\r|;s|;LAYER:$temperatureChangeLayer|;LAYER:$temperatureChangeLayer\rM140 S$fixTemp|" | tr '\r' '\n' >"tmp$parameter" && mv "tmp$parameter" "$parameter"
 done

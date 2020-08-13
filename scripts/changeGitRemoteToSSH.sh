cdb903a90ff40ac86c7f0c78911dbb9990721d5a
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-08-13 20:56:34 +0200

    ADD changeGitRemoteToSSH script

---
+++ scripts/changeGitRemoteToSSH.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+if [ -z "$1" ]; then
+    changeGitRemoteToSSH .
+    exit 0
+else
+    url=$(git -C "$1" config --get remote.origin.url)
+    echo "$url" | grep -o 'git@' >/dev/null && exit 0
+    urlPortion=$(echo "$url" | sed 's/https:\/\/.*\.com\///')
+    domain=$(echo "$url" | grep -o 'https:\/\/.*\.com\/' | sed 's/https:\/\///;s/\//:/')
+    newURL='git@'$domain$urlPortion
+    git -C "$1" remote set-url origin "$newURL"
+fi

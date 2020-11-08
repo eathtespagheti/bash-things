689f6227a40094b2b4fbc77b5d90b2c069ef4681
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-11-08 17:31:23 +0100

    improve linkService script

--- scripts/linkService.sh
+++ scripts/linkService.sh
@@ -1,11 +1,37 @@
 #!/usr/bin/env sh
 
-SYSTEMD_FOLDER="/lib/systemd/system"
+runWithOrWithoutSudo() {
+    if [ "$needSudo" = "true" ]; then
+        eval sudo "$@"
+    else
+        eval "$@"
+    fi
+}
+
+FOLDER="/lib/systemd/system"
+needSudo="true"
+
+# Parse input parameters
+for parameter in "$@"; do
+    case "$parameter" in
+    --user)
+        FOLDER="$XDG_CONFIG_HOME/systemd/user"
+        needSudo="false"
+        ;;
+    *)
+        [ -z "$inputs" ] && inputs="$parameter" || inputs="$inputs $parameter"
+        ;;
+    esac
+done
+
 
 # Check if script exist
 [ ! -f "$BASHTHINGS_SERVICES/$1" ] && echo "Service $1 not found" && exit 2
 
+# Check if folder exist
+[ -d "$FOLDER" ] && runWithOrWithoutSudo mkdir -p "$FOLDER"
+
 # Check if link exist
-[ -f "$SYSTEMD_FOLDER/$1" ] && rm "$SYSTEMD_FOLDER/$1"
+[ -f "$FOLDER/$1" ] && runWithOrWithoutSudo rm "$FOLDER/$1"
 # Make link
-chmod +x "$BASHTHINGS_SERVICES/$1" && ln -s "$BASHTHINGS_SERVICES/$1" "$SYSTEMD_FOLDER/$1"
+chmod +x "$BASHTHINGS_SERVICES/$1" && runWithOrWithoutSudo ln -s "$BASHTHINGS_SERVICES/$1" "$FOLDER/$1"

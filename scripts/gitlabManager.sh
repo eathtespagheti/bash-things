084cc1468418d897ce15ea5f624a2b7701f49852
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-02-07 14:42:21 +0100

    ADD clone function

--- scripts/gitlabManager.sh
+++ scripts/gitlabManager.sh
@@ -5,77 +5,85 @@ CONFIG_FILE_NAME="gitlabManager.config"
 DATA_FOLDER_NAME="gitlabManager"
 PROJECTS_FILE_NAME="projects.json"
 API_ROOT="https://gitlab.com/api/v4"
-VERBOSE="false"
 ACTION="help"
 REPO=""
 
 printHelp() {
-    printf "Parameters:\n\n"
+    printf "Actions:\n\n"
+    printf "%s:\n\t%s\n" "clone, -c <repo-path>" "Clone repo"
+    printf "%s:\n\t%s\n" "list, -l" "List all repos"
+    printf "%s:\n\t%s\n" "update, -u" "Update projects list"
+    printf "\nParameters:\n\n"
     printf "%s:\n\t%s\n" "-h, --help" "Print this message"
     printf "%s:\n\t%s\n" "-v, --verbose" "Enable verbose mode"
     printf "%s:\n\t%s\n" "-ui, --user-id <user id>" "Set USER_ID"
     printf "%s:\n\t%s\n" "-pt, --private-token <token>" "Set PRIVATE_TOKEN"
     printf "%s:\n\t%s\n" "-cf, --config-file <path/to/config/file>" "Use a different config file"
     printf "%s:\n\t%s\n" "-pf, --projects-file <path/to/projects/file>" "Use a different projects file"
-    printf "\nActions:\n\n"
-    printf "%s:\n\t%s\n" "clone, -c <repo-path>" "Clone repo"
-    printf "%s:\n\t%s\n" "list, -l" "List all repos"
-    printf "%s:\n\t%s\n" "update, -u" "Update projects list"
     exit 0
 }
 
 parseArgs() {
+    toShift=""
     for arg in "$@"; do
         case "$arg" in
         -v)
             VERBOSE="true"
-            [ "$VERBOSE" = "true" ] && echo "Enable Verbose"
+            [ -n "$VERBOSE" ] && echo "Enable Verbose"
             shift
             ;;
         --verbose)
             VERBOSE="true"
-            [ "$VERBOSE" = "true" ] && echo "Enable Verbose"
+            [ -n "$VERBOSE" ] && echo "Enable Verbose"
             shift
             ;;
         -ui)
             shift
             USER_ID="$1"
-            [ "$VERBOSE" = "true" ] && echo "Set user id to $USER_ID"
+            toShift="true"
+            [ -n "$VERBOSE" ] && echo "Set user id to $USER_ID"
             ;;
         --user-id)
             shift
             USER_ID="$1"
-            [ "$VERBOSE" = "true" ] && echo "Set user id to $USER_ID"
+            toShift="true"
+            [ -n "$VERBOSE" ] && echo "Set user id to $USER_ID"
             ;;
         -pt)
             shift
             PRIVATE_TOKEN="$1"
-            [ "$VERBOSE" = "true" ] && echo "Set private token to $PRIVATE_TOKEN"
+            toShift="true"
+            [ -n "$VERBOSE" ] && echo "Set private token to $PRIVATE_TOKEN"
             ;;
         --private-token)
             shift
             PRIVATE_TOKEN="$1"
-            [ "$VERBOSE" = "true" ] && echo "Set private token to $PRIVATE_TOKEN"
+            toShift="true"
+            [ -n "$VERBOSE" ] && echo "Set private token to $PRIVATE_TOKEN"
             ;;
         -cf)
             shift
             CONFIG_FILE="$1"
-            [ "$VERBOSE" = "true" ] && echo "Use $CONFIG_FILE as config file"
+            toShift="true"
+            [ -n "$VERBOSE" ] && echo "Use $CONFIG_FILE as config file"
             ;;
         --config-file)
             shift
             CONFIG_FILE="$1"
-            [ "$VERBOSE" = "true" ] && echo "Use $CONFIG_FILE as config file"
+            toShift="true"
+            [ -n "$VERBOSE" ] && echo "Use $CONFIG_FILE as config file"
             ;;
         -pf)
             shift
             PROJECTS_FILE="$1"
-            [ "$VERBOSE" = "true" ] && echo "Use $PROJECTS_FILE as projects file"
+            toShift="true"
+            [ -n "$VERBOSE" ] && echo "Use $PROJECTS_FILE as projects file"
             ;;
         --projects-file)
             shift
             PROJECTS_FILE="$1"
-            [ "$VERBOSE" = "true" ] && echo "Use $PROJECTS_FILE as projects file"
+            toShift="true"
+            [ -n "$VERBOSE" ] && echo "Use $PROJECTS_FILE as projects file"
             ;;
         -h)
             printHelp
@@ -89,11 +97,13 @@ parseArgs() {
             ACTION="clone"
             shift
             REPO="$1"
+            toShift="true"
             ;;
         clone)
             ACTION="clone"
             shift
             REPO="$1"
+            toShift="true"
             ;;
         -l)
             ACTION="list"
@@ -112,6 +122,11 @@ parseArgs() {
             shift
             ;;
         *)
+            [ -z "$toShift" ] && {
+                printf "Unrecognized parameter or action: %s\n\n" "$arg"
+                printHelp
+            }
+            toShift=""
             shift
             ;;
         esac
@@ -138,8 +153,10 @@ initPaths() {
     DATA_FOLDER="$XDG_DATA_HOME/$DATA_FOLDER_NAME"
     PROJECTS_FILE="$DATA_FOLDER/$PROJECTS_FILE_NAME"
 
+    [ -z "$PROJECTS_FOLDER" ] && PROJECTS_FOLDER="."
+
     [ ! -d "$DATA_FOLDER" ] && {
-        [ "$VERBOSE" = "true" ] && echo "Creating data folder in $DATA_FOLDER"
+        [ -n "$VERBOSE" ] && echo "Creating data folder in $DATA_FOLDER"
         mkdir -p "$DATA_FOLDER"
     }
 }
@@ -153,34 +170,34 @@ loadConfigFile() {
         echo "Config file doesn't exist"
         checkUserVariables || exit 1
     }
-    [ "$VERBOSE" = "true" ] && echo "Loading config file"
+    [ -n "$VERBOSE" ] && echo "Loading config file"
     . "$CONFIG_FILE" # Source config file only if user variables aren't already set
 }
 
 updateProjectsList() {
-    [ "$VERBOSE" = "true" ] && echo "Updating projects list in $PROJECTS_FILE"
+    [ -n "$VERBOSE" ] && echo "Updating projects list in $PROJECTS_FILE"
     if [ -n "$PRIVATE_TOKEN" ]; then
         request="$API_ROOT/projects?min_access_level=10"
-        [ "$VERBOSE" = "true" ] && echo "Retrieving public projects from $request"
+        [ -n "$VERBOSE" ] && echo "Retrieving public projects from $request"
         curl --header "Authorization: Bearer $PRIVATE_TOKEN" "$request" -o "$DATA_FOLDER/public.json" >/dev/null 2>&1 || {
             echo "Error getting projects from API"
             exit 2
         }
         request="$request&visibility=private"
-        [ "$VERBOSE" = "true" ] && echo "Retrieving private projects from $request"
+        [ -n "$VERBOSE" ] && echo "Retrieving private projects from $request"
         curl --header "Authorization: Bearer $PRIVATE_TOKEN" "$request" -o "$DATA_FOLDER/private.json" >/dev/null 2>&1 || {
             echo "Error getting projects from API"
             exit 2
         }
     elif [ -n "$USER_ID" ]; then
         request="$API_ROOT/users/$USER_ID/projects"
-        [ "$VERBOSE" = "true" ] && echo "Retrieving public projects from $request"
+        [ -n "$VERBOSE" ] && echo "Retrieving public projects from $request"
         curl "$request" -o "$DATA_FOLDER/public.json" >/dev/null 2>&1 || {
             echo "Error getting projects from API"
             exit 2
         }
         request="$request?visibility=private"
-        [ "$VERBOSE" = "true" ] && echo "Retrieving private projects from $request"
+        [ -n "$VERBOSE" ] && echo "Retrieving private projects from $request"
         curl --header "Authorization: Bearer $PRIVATE_TOKEN" "$request" -o "$DATA_FOLDER/private.json" >/dev/null 2>&1 || {
             echo "Error getting projects from API"
             exit 2
@@ -190,7 +207,7 @@ updateProjectsList() {
         exit 4
     fi
 
-    [ "$VERBOSE" = "true" ] && echo "Merging public and private projects"
+    [ -n "$VERBOSE" ] && echo "Merging public and private projects"
     jq ".[]" "$DATA_FOLDER/public.json" "$DATA_FOLDER/private.json" >"$PROJECTS_FILE"
     rm "$DATA_FOLDER/public.json" "$DATA_FOLDER/private.json"
 }
@@ -203,11 +220,15 @@ printProject() {
     printf "Name: %s\nDescrption: %s\nPath: %s\nURL: %s\n\n" "$name" "$description" "$path" "$url"
 }
 
+fromJsonToList() {
+    jq -r "[.name, .description, .path_with_namespace, .ssh_url_to_repo] | @tsv" "$PROJECTS_FILE"
+}
+
 listProjects() {
-    [ "$VERBOSE" = "true" ] && echo "Checking if projects file exist"
+    [ -n "$VERBOSE" ] && echo "Checking if projects file exist"
     [ ! -f "$PROJECTS_FILE" ] && updateProjectsList
 
-    jq -r "[.name, .description, .path_with_namespace, .ssh_url_to_repo] | @tsv" "$PROJECTS_FILE" |
+    fromJsonToList |
         while IFS="" read -r line || [ -n "$line" ]; do
             printProject "$line"
         done
@@ -215,7 +236,8 @@ listProjects() {
 }
 
 cloneProject() {
-    echo "Cloning $REPO"
+    URL=$(fromJsonToList | grep "$REPO" | cut -f 4)
+    git -C "$PROJECTS_FOLDER" clone --recurse-submodules "$URL"
 }
 
 execAction() {

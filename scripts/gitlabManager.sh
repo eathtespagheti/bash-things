492626f87f12fceb240cb8c651869172dea3998b
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-02-07 13:40:13 +0100

    gitlabManager ADD Args parsing and help

--- scripts/gitlabManager.sh
+++ scripts/gitlabManager.sh
@@ -4,6 +4,91 @@
 CONFIG_FILE_NAME="gitlabManager.config"
 DATA_FOLDER_NAME="gitlabManager"
 API_ROOT="https://gitlab.com/api/v4"
+VERBOSE="false"
+ACTION="help"
+REPO=""
+
+printHelp() {
+    printf "Parameters:\n\n"
+    printf "%s:\n\t%s\n" "-h, --help" "Print this message"
+    printf "%s:\n\t%s\n" "-v, --verbose" "Enable verbose mode"
+    printf "%s:\n\t%s\n" "-ui, --user-id" "Set USER_ID"
+    printf "%s:\n\t%s\n" "-pt, --private-token" "Set PRIVATE_TOKEN"
+    printf "\nActions:\n\n"
+    printf "%s:\n\t%s\n" "clone, -c <repo-path>" "Clone repo"
+    printf "%s:\n\t%s\n" "list, -l" "List all repos"
+    printf "%s:\n\t%s\n" "update, -u" "Update projects list"
+    exit 0
+}
+
+parseArgs() {
+    for arg in "$@"; do
+        case "$arg" in
+        -v)
+            VERBOSE="true"
+            shift
+            ;;
+        --verbose)
+            VERBOSE="true"
+            shift
+            ;;
+        -ui)
+            shift
+            USER_ID="$1"
+            ;;
+        --user-id)
+            shift
+            USER_ID="$1"
+            ;;
+        -pt)
+            shift
+            PRIVATE_TOKEN="$1"
+            ;;
+        --private-token)
+            shift
+            PRIVATE_TOKEN="$1"
+            ;;
+        -h)
+            printHelp
+            shift
+            ;;
+        --help)
+            printHelp
+            shift
+            ;;
+        -c)
+            ACTION="clone"
+            shift
+            REPO="$1"
+            ;;
+        clone)
+            ACTION="clone"
+            shift
+            REPO="$1"
+            ;;
+        -l)
+            ACTION="list"
+            shift
+            ;;
+        list)
+            ACTION="list"
+            shift
+            ;;
+        -u)
+            ACTION="update"
+            shift
+            ;;
+        update)
+            ACTION="update"
+            shift
+            ;;
+        *)
+            shift
+            ;;
+        esac
+
+    done
+}
 
 checkDependencies() {
     command -v curl >/dev/null 2>&1 || {
@@ -23,52 +108,91 @@ initPaths() {
     [ -z "$XDG_DATA_HOME" ] && XDG_DATA_HOME="$HOME/.local/share"
     DATA_FOLDER="$XDG_DATA_HOME/$DATA_FOLDER_NAME"
 
-    [ ! -d "$DATA_FOLDER" ] && mkdir -p "$DATA_FOLDER"
+    [ ! -d "$DATA_FOLDER" ] && {
+        [ "$VERBOSE" = "true" ] && echo "Creating data folder in $DATA_FOLDER"
+        mkdir -p "$DATA_FOLDER"
+    }
 }
 
 checkUserVariables() {
     [ -n "$USER_ID" ] || [ -n "$PRIVATE_TOKEN" ]
 }
 
+loadConfigFile() {
+    [ ! -f "$CONFIG_FILE" ] && {
+        echo "Config file doesn't exist"
+        checkUserVariables || exit 1
+    }
+    [ "$VERBOSE" = "true" ] && echo "Loading config file"
+    checkUserVariables || . "$CONFIG_FILE" # Source config file only if user variables aren't already set
+}
+
 updateProjectsList() {
     [ -n "$USER_ID" ] && {
         request="$API_ROOT/users/$USER_ID/projects"
+        [ "$VERBOSE" = "true" ] && echo "Retrieving public projects from $request"
         curl "$request" -o "$DATA_FOLDER/public.json" >/dev/null 2>&1 || {
             echo "Error getting projects from API"
             exit 2
         }
         request="$request?visibility=private"
+        [ "$VERBOSE" = "true" ] && echo "Retrieving private projects from $request"
         curl --header "Authorization: Bearer $PRIVATE_TOKEN" "$request" -o "$DATA_FOLDER/private.json" >/dev/null 2>&1 || {
             echo "Error getting projects from API"
             exit 2
         }
     }
+    [ "$VERBOSE" = "true" ] && echo "Merging public and private projects"
     jq ".[]" "$DATA_FOLDER/public.json" "$DATA_FOLDER/private.json" >"$DATA_FOLDER/projects.json"
     rm "$DATA_FOLDER/public.json" "$DATA_FOLDER/private.json"
 }
 
+printProject() {
+    name=$(echo "$line" | cut -f 1)
+    description=$(echo "$line" | cut -f 2)
+    path=$(echo "$line" | cut -f 3)
+    url=$(echo "$line" | cut -f 4)
+    printf "Name: %s\nDescrption: %s\nPath: %s\nURL: %s\n\n" "$name" "$description" "$path" "$url"
+}
+
 listProjects() {
+    [ "$VERBOSE" = "true" ] && echo "Checking if projects file exist"
     [ ! -f "$DATA_FOLDER/projects.json" ] && updateProjectsList
 
-    jq -r "[.name, .description, .path, .ssh_url_to_repo] | @tsv" "$DATA_FOLDER/projects.json" |
+    jq -r "[.name, .description, .path_with_namespace, .ssh_url_to_repo] | @tsv" "$DATA_FOLDER/projects.json" |
         while IFS="" read -r line || [ -n "$line" ]; do
-            name=$(echo "$line" | cut -f 1)
-            description=$(echo "$line" | cut -f 2)
-            path=$(echo "$line" | cut -f 3)
-            url=$(echo "$line" | cut -f 4)
-            printf "Name: %s\nDescrption: %s\nPath: %s\nURL: %s\n\n" "$name" "$description" "$path" "$url"
+            printProject "$line"
         done
+
+}
+
+cloneProject() {
+    echo "Cloning $REPO"
+}
+
+execAction() {
+    case "$ACTION" in
+    list)
+        listProjects
+        ;;
+    help)
+        printHelp
+        ;;
+    clone)
+        cloneProject
+        ;;
+    update)
+        updateProjectsList
+        ;;
+    *)
+        listProjects
+        ;;
+    esac
 
 }
 
 initPaths
 checkDependencies
-
-[ ! -f "$CONFIG_FILE" ] && {
-    echo "Config file doesn't exist"
-    checkUserVariables || exit 1
-}
-checkUserVariables || . "$CONFIG_FILE" # Source config file only if user variables aren't already set
-
-[ ! -f "$DATA_FOLDER/projects.json" ] && updateProjectsList
-listProjects
+parseArgs "$@"
+loadConfigFile
+execAction

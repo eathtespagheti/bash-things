f432be14537cf07525934f7e8ff0e585978e53ea
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-02-26 16:02:23 +0100

    Improve mineETH script

--- scripts/mineETH.sh
+++ scripts/mineETH.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+[ "$1" = "-s" ] && sudo amdvbflash -p 0 ~/stockBIOS.rom && reboot
+[ "$1" = "-m" ] && sudo amdvbflash -p 0 ~/miningBIOS.rom && reboot
+
 sudo AlterGPUClocks
 sudo setGPUfanSpeed 35
 sudo systemctl start ethermine

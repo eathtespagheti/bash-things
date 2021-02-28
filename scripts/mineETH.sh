bfe58dd4eb08dfc1e480cbc94da6735123e08b07
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-02-28 17:50:55 +0100

    ADD monitoring parameter

--- scripts/mineETH.sh
+++ scripts/mineETH.sh
@@ -2,8 +2,8 @@
 
 [ "$1" = "-s" ] && sudo amdvbflash -p 0 ~/stockBIOS.rom && reboot
 [ "$1" = "-m" ] && sudo amdvbflash -p 0 ~/miningBIOS.rom && reboot
+[ "$1" = "-p" ] && watch sudo systemctl status ethermine.service && exit
 
 amdgpuclocks mining
 sudo setGPUfanSpeed 28
 sudo systemctl start ethermine
-watch sudo systemctl status ethermine.service

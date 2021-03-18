bd9b8be16eb1f20c8febe69493a1229d26bad43e
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-03-18 14:24:44 +0100

    ADD date polito script

---
+++ scripts/datePolito.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+for file in "$@"; do
+    date="$(echo "$file" | cut -d ' ' -f 3)"
+    year="$(echo "$date" | cut -c 5-8)"
+    month="$(echo "$date" | cut -c 3-4)"
+    day="$(echo "$date" | cut -c 1-2)"
+    newdate="$year$month$day"
+    newname="$(echo "$file" | sed "s/$date/$newdate/")"
+done

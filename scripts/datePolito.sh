8e14b021fa5826f5116396579ccc8b4fd1b62de1
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-03-19 18:25:47 +0100

    improve datepolito script

--- scripts/datePolito.sh
+++ scripts/datePolito.sh
@@ -2,10 +2,9 @@
 
 for file in "$@"; do
     date="$(echo "$file" | cut -d ' ' -f 3)"
-    year="$(echo "$date" | cut -c 5-8)"
     month="$(echo "$date" | cut -c 3-4)"
     day="$(echo "$date" | cut -c 1-2)"
-    newdate="$year$month$day"
+    newdate="$month$day"
     newname="$(echo "$file" | sed "s/$date/$newdate/")"
     echo "from $file to $newname"
     mv "$file" "$newname"

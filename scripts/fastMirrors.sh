77d1e4ef950a7e61035c38b50632b3108127b37f
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-05-13 14:04:33 +0200

    fix fastmirrors

--- scripts/fastMirrors.sh
+++ scripts/fastMirrors.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 curl -s "https://archlinux.org/mirrorlist/?country=IT&country=FR&country=ES&protocol=https&ip_version=4" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 0 - >/etc/pacman.d/mirrorlist
-sed -i "1i Server = https://repo.eathtehspagheti.eu" "/etc/pacman.d/mirrorlist"
+sed -i "1i Server = https://repo.eathtespagheti.eu" "/etc/pacman.d/mirrorlist"

e69d8c77b32f34363cea6f9190b16070d7b7f184
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-05-13 14:35:12 +0200

    separate findGPU function

--- scripts/amdgpuclocks.sh
+++ scripts/amdgpuclocks.sh
@@ -1,40 +1,43 @@
 #!/usr/bin/env sh
 
 amd_vendor="0x1002"
-
-# Find AMD GPU
-GPU_base="card"
-GPU="card0"
-i=1
-while [ "$i" -ne 10 ] && [ ! "$(cat /sys/class/drm/$GPU/device/vendor)" = "$amd_vendor" ]; do
-    GPU="$GPU_base$i"
-    i=$((i + 1))
-done
-
-[ ! "$(cat /sys/class/drm/$GPU/device/vendor)" = "$amd_vendor" ] && return 1
-
-device_path="/sys/class/drm/$GPU/device"
-pp_od_clk_voltage_path="$device_path/pp_od_clk_voltage"
-pp_power_profile_mode_path="$device_path/pp_power_profile_mode"
-power_dpm_force_performance_level_path="$device_path/power_dpm_force_performance_level"
-
-# Find HWMON path
-HWMON_base="hwmon"
-HWMON="$HWMON_base"0
-i=1
-while [ "$i" -ne 10 ] && [ ! -d "$device_path/hwmon/$HWMON" ]; do
-    HWMON="$HWMON_base$i"
-    i=$((i + 1))
-done
-
-AMDGPU_POWERCAP="$device_path/hwmon/$HWMON/power1_cap"
-
 CONFIG_FOLDER_NAME="amdgpuclocks"
 [ -z "$XDG_CONFIG_HOME" ] && XDG_CONFIG_HOME="$HOME/.config"
 CONFIG_FOLDER="$XDG_CONFIG_HOME/$CONFIG_FOLDER_NAME"
 GLOBAL_CONFIG_FOLDER="/etc/$CONFIG_FOLDER_NAME"
 
+findGPU() {
+    # Find AMD GPU
+    GPU_base="card"
+    GPU="card0"
+    i=1
+    while [ "$i" -ne 10 ] && [ ! "$(cat /sys/class/drm/$GPU/device/vendor)" = "$amd_vendor" ]; do
+        GPU="$GPU_base$i"
+        i=$((i + 1))
+    done
+
+    [ ! "$(cat /sys/class/drm/$GPU/device/vendor)" = "$amd_vendor" ] && return 1
+
+    device_path="/sys/class/drm/$GPU/device"
+    pp_od_clk_voltage_path="$device_path/pp_od_clk_voltage"
+    pp_power_profile_mode_path="$device_path/pp_power_profile_mode"
+    power_dpm_force_performance_level_path="$device_path/power_dpm_force_performance_level"
+
+    # Find HWMON path
+    HWMON_base="hwmon"
+    HWMON="$HWMON_base"0
+    i=1
+    while [ "$i" -ne 10 ] && [ ! -d "$device_path/hwmon/$HWMON" ]; do
+        HWMON="$HWMON_base$i"
+        i=$((i + 1))
+    done
+
+    AMDGPU_POWERCAP="$device_path/hwmon/$HWMON/power1_cap"
+}
+
 apply_oc() {
+    findGPU
+    
     oc="$(echo "$1" | grep "^[^pc] ")"
     power_cap="$(echo "$1" | grep "^[^smp] " | cut -d ' ' -f 2)"
     power_profile="$(echo "$1" | grep "^[^smc] " | cut -d ' ' -f 2)"

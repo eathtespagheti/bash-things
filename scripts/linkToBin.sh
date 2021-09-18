937506dc69137419ec9838c6a08f9b7304beec09
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-09-18 20:06:19 +0200

    change return to exit

--- scripts/linkToBin.sh
+++ scripts/linkToBin.sh
@@ -2,11 +2,11 @@
 
 # Create bin name
 binName=$(echo "$1" | cut -f 1 -d '.')
-[ -z "$binName" ] && echo "Unable to parse the scriptname" && return 1
+[ -z "$binName" ] && echo "Unable to parse the scriptname" && exit 1
 
 # Check if script exist
-[ ! -f "$BASHTHINGS_SCRIPTS/$1" ] && echo "Script $1 not found" && return 2
+[ ! -f "$BASHTHINGS_SCRIPTS/$1" ] && echo "Script $1 not found" && exit 2
 
 # Make link
-cd "$BASHTHINGS_BIN" || return 1
+cd "$BASHTHINGS_BIN" || exit 1
 chmod +x "../$1" && ln -f -s "../$1" "$binName"

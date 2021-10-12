bc0ae3cae54e57615c03a5c6828d59bbd17bf3c5
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-10-12 10:01:30 +0200

    update loadvirtualenvwrapper

--- scripts/virtualenvwrapper.bash
+++ scripts/virtualenvwrapper.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-export WORKON_HOME=$HOME/.virtualenvs && export PROJECT_HOME=$HOME/Projects && source $HOME/.local/bin/virtualenvwrapper.sh
+export WORKON_HOME=$HOME/.virtualenvs && export PROJECT_HOME=$HOME/Projects && source /usr/bin/virtualenvwrapper.sh

30fee503e36bf6721b9094a38a093f948d7dd078
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2025-03-28 22:38:13 +0100

    add new headphones to virtualsink script

--- scripts/virtualsink.sh
+++ scripts/virtualsink.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env sh
 
 VIRTUAL_SINK_NAME="BluetoothVirtualSink"
-BT_DEVICE_1="bluez_output.7C_C9_5E_B4_51_49.1"
-BT_DEVICE_2="bluez_output.18_B9_6E_CD_B3_F8.1"
+
+# Bluetooth adresses
+BLUETOOTH_ADDRESSES=""
+# Redmi Buds 4
+BLUETOOTH_ADDRESSES="7C:C9:5E:B4:51:49 $BLUETOOTH_ADDRESSES"
+# Redmi Buds 6 Pro
+BLUETOOTH_ADDRESSES="00:BB:43:AA:8F:A6 $BLUETOOTH_ADDRESSES"
+# Redmi Buds 5 Pro
+BLUETOOTH_ADDRESSES="48:73:CB:A4:D8:6B $BLUETOOTH_ADDRESSES"
+
 
 # Create a virtual sink
 pactl list sinks | grep "$VIRTUAL_SINK_NAME" || {
@@ -10,10 +18,16 @@ pactl list sinks | grep "$VIRTUAL_SINK_NAME" || {
     pactl load-module module-null-sink media.class=Audio/Sink sink_name="$VIRTUAL_SINK_NAME" channel_map=stereo
 }
 
-# Connect the bluetooth devices to the virtual sink
+# Get list of connected devices
+connected_devices="$(bluetoothctl devices Connected | cut -f 2 -d ' ')"
 
-pw-link "$VIRTUAL_SINK_NAME":monitor_FL "$BT_DEVICE_1":playback_FL
-pw-link "$VIRTUAL_SINK_NAME":monitor_FR "$BT_DEVICE_1":playback_FR
-
-pw-link "$VIRTUAL_SINK_NAME":monitor_FL "$BT_DEVICE_2":playback_FL
-pw-link "$VIRTUAL_SINK_NAME":monitor_FR "$BT_DEVICE_2":playback_FR
+for headphone in $BLUETOOTH_ADDRESSES; do
+    # Check if the device is connected
+    echo "$connected_devices" | grep "$headphone" || continue
+    # Create the device address
+    headphone="$(echo "$headphone" | tr ':' '_')"
+    bluez_address="$(printf "bluez_output.%s.1" "$headphone")"
+    # Connect the bluetooth devices to the virtual sink    
+    pw-link "$VIRTUAL_SINK_NAME":monitor_FL "$bluez_address":playback_FL
+    pw-link "$VIRTUAL_SINK_NAME":monitor_FR "$bluez_address":playback_FR
+done
